1fd187fe1a1e4c9dd718da12693fc0c735f16133
Hugues Maignol <hugues@kime.tech>
2023-04-13 09:00:37 +0200

    qa: remove debug code

--- apps/web/src/app/(public)/page.tsx
+++ apps/web/src/app/(public)/page.tsx
@@ -1,13 +1,3 @@
 export default async function HomePage() {
-  const revalidationNonce = Math.floor(Math.random() * 10_000).toString()
-
-  return (
-    <>
-      <div
-        data-revalidation={revalidationNonce}
-        className="fr-background-alt--blue-france"
-        style={{ position: 'relative' }}
-      />
-    </>
-  )
+  return (<div className='fr-container'/>)
 }
