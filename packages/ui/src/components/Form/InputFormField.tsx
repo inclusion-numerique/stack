792b60d470b7b84400c3e954e11becf6afe320e7
Desoindx <xavier.desoindre@hotmail.fr>
2023-04-14 12:25:13 +0200

    Add radio

--- packages/ui/src/components/Form/InputFormField.tsx
+++ packages/ui/src/components/Form/InputFormField.tsx
@@ -1,20 +1,33 @@
 import { UiComponentProps } from '@stack/ui/utils/uiComponentProps'
 import classNames from 'classnames'
-import React, { HTMLInputTypeAttribute } from 'react'
+import React, { HTMLInputTypeAttribute, HTMLProps } from 'react'
 import { Control, Controller, FieldValues } from 'react-hook-form'
 import { FieldPath } from 'react-hook-form/dist/types/path'
 
-export type InputFormFieldProps<T extends FieldValues> = {
+type CommonProps<T extends FieldValues> = {
   control: Control<T>
   path: FieldPath<T>
   disabled?: boolean
   label?: string
   hint?: string
-  type?: Exclude<HTMLInputTypeAttribute, 'checkbox'>
   placeholder?: string
   valid?: string
   icon?: string
 }
+
+type InputProps = {
+  type?: Exclude<HTMLInputTypeAttribute, 'checkbox' | 'textarea'>
+}
+
+type TextareaProps = {
+  type: 'textarea'
+} & Omit<
+  HTMLProps<HTMLTextAreaElement>,
+  'onChange' | 'onBlur' | 'value' | 'ref' | 'id' | 'aria-describedby'
+>
+
+export type InputFormFieldProps<T extends FieldValues> = CommonProps<T> &
+  (InputProps | TextareaProps)
 
 const InputFormField = <T extends FieldValues>({
   label,
@@ -28,6 +41,7 @@ const InputFormField = <T extends FieldValues>({
   'data-testid': dataTestId,
   valid,
   icon,
+  ...rest
 }: UiComponentProps & InputFormFieldProps<T>) => {
   const id = `input-form-field__${path}`
 
@@ -63,19 +77,35 @@ const InputFormField = <T extends FieldValues>({
               {hint && <span className="fr-hint-text">{hint}</span>}
             </label>
             <div className={icon ? `fr-input-wrap ${icon}` : ''}>
-              <input
-                data-testid={dataTestId}
-                className="fr-input"
-                aria-describedby={ariaDescribedBy}
-                disabled={disabled}
-                type={type}
-                id={id}
-                placeholder={placeholder}
-                onBlur={onBlur}
-                onChange={onChange}
-                value={value ?? ''}
-                ref={ref}
-              />
+              {type === 'textarea' ? (
+                <textarea
+                  data-testid={dataTestId}
+                  className="fr-input"
+                  aria-describedby={ariaDescribedBy}
+                  disabled={disabled}
+                  id={id}
+                  placeholder={placeholder}
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  value={value ?? ''}
+                  ref={ref}
+                  {...rest}
+                />
+              ) : (
+                <input
+                  data-testid={dataTestId}
+                  className="fr-input"
+                  aria-describedby={ariaDescribedBy}
+                  disabled={disabled}
+                  type={type}
+                  id={id}
+                  placeholder={placeholder}
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  value={value ?? ''}
+                  ref={ref}
+                />
+              )}
             </div>
             {error && (
               <p id={`${id}__error`} className="fr-error-text">
---
+++ packages/ui/src/components/Form/RadioFormField.tsx
@@ -0,0 +1,137 @@
+import React from 'react'
+import classNames from 'classnames'
+import { Control, Controller, FieldValues } from 'react-hook-form'
+import { FieldPath } from 'react-hook-form/dist/types/path'
+import { UiComponentProps } from '@stack/ui/utils/uiComponentProps'
+import { Options } from './utils/options'
+
+export type RadioFormFieldProps<T extends FieldValues> = {
+  control: Control<T>
+  path: FieldPath<T>
+  options: Options
+  disabled?: boolean
+  label?: string
+  hint?: string
+  inline?: boolean
+  valid?: string
+  small?: boolean
+}
+
+const RadioFormField = <T extends FieldValues>({
+  label,
+  path,
+  options,
+  control,
+  hint,
+  disabled,
+  inline,
+  valid,
+  small,
+  className,
+  'data-testid': dataTestId,
+}: UiComponentProps & RadioFormFieldProps<T>) => {
+  const id = `input-form-field__${path}`
+
+  return (
+    <Controller
+      control={control}
+      name={path}
+      render={({
+        field: { onChange, onBlur, name, ref },
+        fieldState: { invalid, error, isDirty },
+      }) => {
+        let ariaLabelBy: string | undefined
+        if (error) {
+          ariaLabelBy = `${id}__error`
+        } else if (valid && isDirty && !invalid) {
+          ariaLabelBy = `${id}__valid`
+        }
+
+        return (
+          <div className="fr-form-group" data-testid={dataTestId}>
+            <fieldset
+              className={classNames(
+                'fr-fieldset',
+                {
+                  'fr-fieldset--error': error,
+                  'fr-fieldset--disabled': disabled,
+                  'fr-fieldset--valid': isDirty && !invalid,
+                },
+                className,
+              )}
+              aria-labelledby={`${id}__legend${
+                ariaLabelBy ? ` ${ariaLabelBy}` : ''
+              }`}
+              role="group"
+            >
+              <legend
+                className="fr-fieldset__legend fr-fieldset__legend--regular"
+                id={`${id}__legend`}
+              >
+                {label}
+                {hint ? <span className="fr-hint-text">{hint}</span> : null}
+              </legend>
+              {options.map((option, index) => (
+                <div
+                  key={option.value}
+                  className={classNames('fr-fieldset__element', {
+                    'fr-fieldset__element--inline': inline,
+                  })}
+                >
+                  <div
+                    className={classNames('fr-radio-group', {
+                      'fr-radio-group--sm': small,
+                    })}
+                  >
+                    <input
+                      type="radio"
+                      id={`${id}__${index}`}
+                      disabled={disabled}
+                      onBlur={onBlur}
+                      onChange={(event) => {
+                        if (event.target.checked) {
+                          onChange(option.value)
+                        }
+                      }}
+                      value={option.value}
+                      name={name}
+                      ref={ref}
+                    />
+                    <label className="fr-label" htmlFor={`${id}__${index}`}>
+                      {option.name}
+                      {option.hint && (
+                        <span className="fr-hint-text">{option.hint}</span>
+                      )}
+                    </label>
+                  </div>
+                </div>
+              ))}
+              {error && (
+                <div
+                  className="fr-messages-group"
+                  id={`${id}__error`}
+                  aria-live="assertive"
+                >
+                  <p className="fr-message fr-message--error">
+                    {error.message}
+                  </p>
+                </div>
+              )}
+              {valid && isDirty && !invalid && (
+                <div
+                  className="fr-messages-group"
+                  id={`${id}__valid`}
+                  aria-live="assertive"
+                >
+                  <p className="fr-message fr-message--valid">{valid}</p>
+                </div>
+              )}
+            </fieldset>
+          </div>
+        )
+      }}
+    />
+  )
+}
+
+export default RadioFormField
