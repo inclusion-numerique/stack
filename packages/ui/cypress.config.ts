fa4659834561ba036cf97cad92de91e32d9a78ab
Hugues Maignol <hugues@kime.tech>
2023-04-18 10:28:21 +0200

    ci: cypress component testing setup

--- packages/ui/cypress.config.ts
+++ packages/ui/cypress.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
+import { cypressProjectId } from '../../packages/config/src/config'
 
 export default defineConfig({
+  projectId: cypressProjectId,
   component: {
     devServer: {
-      framework: "next",
-      bundler: "webpack",
+      framework: 'next',
+      bundler: 'webpack',
     },
   },
-});
+})
