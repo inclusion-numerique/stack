0141db8f2c3183aa7c056f23d5e322c6bfa0a9b3
Hugues Maignol <hugues@kime.tech>
2023-05-04 10:54:15 +0200

    feat: info text in input field

--- packages/ui/src/components/Form/InputFormField.tsx
+++ packages/ui/src/components/Form/InputFormField.tsx
@@ -1,7 +1,13 @@
 import { UiComponentProps } from '@app/ui/utils/uiComponentProps'
 import classNames from 'classnames'
 import React, { HTMLInputTypeAttribute, HTMLProps } from 'react'
-import { Control, Controller, FieldValues } from 'react-hook-form'
+import {
+  Control,
+  Controller,
+  FieldValues,
+  Path,
+  PathValue,
+} from 'react-hook-form'
 import { FieldPath } from 'react-hook-form/dist/types/path'
 
 type CommonProps<T extends FieldValues> = {
@@ -13,6 +19,7 @@ type CommonProps<T extends FieldValues> = {
   placeholder?: string
   valid?: string
   icon?: string
+  info?: string | ((value: PathValue<T, Path<T>>) => string)
 }
 
 type InputProps = {
@@ -41,6 +48,7 @@ const InputFormField = <T extends FieldValues>({
   'data-testid': dataTestId,
   valid,
   icon,
+  info,
   ...rest
 }: UiComponentProps & InputFormFieldProps<T>) => {
   const id = `input-form-field__${path}`
@@ -60,6 +68,37 @@ const InputFormField = <T extends FieldValues>({
           ariaDescribedBy = `${id}__valid`
         }
 
+        const input =
+          type === 'textarea' ? (
+            <textarea
+              data-testid={dataTestId}
+              className="fr-input"
+              aria-describedby={ariaDescribedBy}
+              disabled={disabled}
+              id={id}
+              placeholder={placeholder}
+              onBlur={onBlur}
+              onChange={onChange}
+              value={value ?? ''}
+              ref={ref}
+              {...rest}
+            />
+          ) : (
+            <input
+              data-testid={dataTestId}
+              className="fr-input"
+              aria-describedby={ariaDescribedBy}
+              disabled={disabled}
+              type={type}
+              id={id}
+              placeholder={placeholder}
+              onBlur={onBlur}
+              onChange={onChange}
+              value={value ?? ''}
+              ref={ref}
+            />
+          )
+
         return (
           <div
             className={classNames(
@@ -72,48 +111,33 @@ const InputFormField = <T extends FieldValues>({
               className,
             )}
           >
-            <label className="fr-label" htmlFor={id}>
+            <label className="fr-label fr-mb-1v" htmlFor={id}>
               {label}
               {hint && <span className="fr-hint-text">{hint}</span>}
             </label>
-            <div className={icon ? `fr-input-wrap ${icon}` : ''}>
-              {type === 'textarea' ? (
-                <textarea
-                  data-testid={dataTestId}
-                  className="fr-input"
-                  aria-describedby={ariaDescribedBy}
-                  disabled={disabled}
-                  id={id}
-                  placeholder={placeholder}
-                  onBlur={onBlur}
-                  onChange={onChange}
-                  value={value ?? ''}
-                  ref={ref}
-                  {...rest}
-                />
-              ) : (
-                <input
-                  data-testid={dataTestId}
-                  className="fr-input"
-                  aria-describedby={ariaDescribedBy}
-                  disabled={disabled}
-                  type={type}
-                  id={id}
-                  placeholder={placeholder}
-                  onBlur={onBlur}
-                  onChange={onChange}
-                  value={value ?? ''}
-                  ref={ref}
-                />
-              )}
-            </div>
+            {icon ? (
+              <div className={`fr-input-wrap ${icon}`}>{input}</div>
+            ) : (
+              input
+            )}
+            {info && (
+              <p id={`${id}__info`} className="fr-hint-text fr-mt-1v fr-mb-0">
+                {typeof info === 'string' ? info : info(value)}
+              </p>
+            )}
             {error && (
-              <p id={`${id}__error`} className="fr-error-text">
+              <p
+                id={`${id}__error`}
+                className={classNames('fr-error-text', { 'fr-mt-1v': !!info })}
+              >
                 {error.message}
               </p>
             )}
             {valid && isTouched && !invalid && (
-              <p id={`${id}__valid`} className="fr-valid-text">
+              <p
+                id={`${id}__valid`}
+                className={classNames('fr-valid-text', { 'fr-mt-1v': !!info })}
+              >
                 {valid}
               </p>
             )}
