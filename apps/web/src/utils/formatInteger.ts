4ba20f91add86691a893c52f3256279764f461b9
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2023-05-11 10:13:24 +0200

    chore: Update packages (#18)

--- apps/web/src/utils/formatInteger.ts
+++ apps/web/src/utils/formatInteger.ts
@@ -2,5 +2,5 @@ export const formatInteger = (value: number) => {
   if (value < 10_000) {
     return value.toString()
   }
-  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+  return value.toString().replaceAll(/\B(?=(\d{3})+(?!\d))/g, ' ')
 }
