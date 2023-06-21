b39ca521d9d6a34992932b1c862c780d200cbdf8
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2023-06-21 22:58:52 +0200

    Fix: Align signup and signin panel width (#35)

--- apps/web/src/app/(public)/(authentication)/AuthCard.tsx
+++ apps/web/src/app/(public)/(authentication)/AuthCard.tsx
@@ -1,17 +1,14 @@
 import classNames from 'classnames'
 import { PropsWithChildren } from 'react'
 
-export const AuthCard = ({
-  children,
-  wide,
-}: PropsWithChildren<{ wide?: boolean }>) => (
+export const AuthCard = ({ children }: PropsWithChildren) => (
   <main role="main" id="content">
     <div className="fr-grid-row fr-grid-row--center fr-mb-md-12v fr-mt-12v">
       <div
         className={classNames(
           'fr-col-12',
-          wide ? 'fr-col-md-10' : 'fr-col-md-8',
-          wide ? 'fr-col-lg-8' : 'fr-col-lg-6',
+          'fr-col-md-8',
+          'fr-col-lg-6',
           'fr-background-alt--grey',
           'fr-px-4v',
           'fr-py-4v',
