3f9b60141140803a9b624151206017e2e5dcb4ed
Hugues Maignol <hugues@kime.tech>
2023-07-13 13:37:01 +0200

    feat: this prefect does not exist

--- apps/web/src/app/(public)/page.tsx
+++ apps/web/src/app/(public)/page.tsx
@@ -1,5 +1,6 @@
 import Button from '@codegouvfr/react-dsfr/Button'
 import Image from 'next/image'
+import Quote from '@codegouvfr/react-dsfr/Quote'
 import styles from './Home.module.css'
 
 export const revalidate = 0
@@ -10,12 +11,13 @@ const HomePage = () => (
       <h1>Les données de l&apos;Inclusion Numérique</h1>
       <h3 className="fr-mt-12v">Œuvrer Ensemble pour un Numérique Inclusif</h3>
       <p className="fr-mt-12v">
-        L’ANCT, en accord avec la feuille de route France Numérique Ensemble
+        L’ANCT, dans le cadre de la feuille de route France Numérique Ensemble
         issue du Conseil National de la Refondation, vise à soutenir les acteurs
         territoriaux en fournissant des outils de diagnostic quantitatif et en
         développant des stratégies d’inclusion numérique.
       </p>
-      <p className="fr-mb-2v">En naviguant sur ce site, vous pouvez&nbsp;:</p>
+
+      <p className="fr-mb-2v">Principales fonctionnalités&nbsp;:</p>
       <ul className="fr-mb-4v">
         <li>
           Accéder à une analyse détaillée par département sur l’inclusion
@@ -28,14 +30,14 @@ const HomePage = () => (
         </li>
         <li>
           Participer à un effort commun pour renforcer l’inclusion numérique et
-          améliorer l’accès aux services pour tous.
+          améliorer l’accès aux services publics pour tous.
         </li>
       </ul>
       <p>
         Initialement dédié aux services préfectoraux, ce site sera
-        progressivement étendu à tous les acteurs territoriaux, soulignant le
-        rôle essentiel des collectivités locales dans la mise en œuvre de
-        l’inclusion numérique.
+        progressivement étendu à tous les acteurs territoriaux pour la
+        coordination de la mise en œuvre de la feuille de route France Numérique
+        Ensemble.
       </p>
       <Button
         className="fr-mt-12v"
@@ -54,6 +56,23 @@ const HomePage = () => (
         className={styles.homeImage}
         width={972}
         height={1032}
+      />
+    </div>
+    <div className="fr-col-12 fr-mt-8v">
+      <Quote
+        text="La Plateforme de Cohésion Numérique offre un panorama sans précédent de notre territoire. Elle nous permet de prioriser efficacement nos initiatives et d'agir où l'action est le plus nécessaire."
+        imageUrl="/images/prefet/prefet-a.png"
+        author="Sophie Martin, Préfète de Mont-Royal"
+      />
+      <Quote
+        text="Ce qui est remarquable avec cette plateforme, c'est sa capacité à fournir des données actualisées et pertinentes. C'est un outil inestimable pour planifier et suivre le progrès de l'inclusion numérique."
+        imageUrl="/images/prefet/prefet-b.png"
+        author="Jean Dupont, Préfet de Saint-Claire"
+      />
+      <Quote
+        text="En rendant les données accessibles à tous les acteurs territoriaux, la Plateforme de Cohésion Numérique favorise une véritable collaboration. C'est un pas en avant considérable pour notre gouvernance locale."
+        imageUrl="/images/prefet/prefet-c.png"
+        author="Claire Dubois, Préfète de Verdania"
       />
     </div>
   </div>
