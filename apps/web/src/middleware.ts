afe4d9c788e1a4df26b757d099c92da2c9e7a508
Hugues Maignol <hugues@kime.tech>
2023-07-28 16:37:20 +0200

    fix: connect-src with no bucket configured

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -13,9 +13,11 @@ const contentSecurityPolicy = `
   img-src 'self' data:;
   frame-src https://www.youtube-nocookie.com/;
   object-src 'none';
-  connect-src 'self' https://${ServerWebAppConfig.S3.uploadsBucket}.${
-  ServerWebAppConfig.S3.host
-} https://matomo.incubateur.anct.gouv.fr https://sentry.incubateur.net https://openmaptiles.geo.data.gouv.fr https://openmaptiles.github.io https://aides-territoires.beta.gouv.fr https://openmaptiles.geo.data.gouv.fr https://wxs.ign.fr https://geo.api.gouv.fr;
+  connect-src 'self' ${
+    ServerWebAppConfig.S3.uploadsBucket
+      ? `https://${ServerWebAppConfig.S3.uploadsBucket}.${ServerWebAppConfig.S3.host}`
+      : ''
+  } https://matomo.incubateur.anct.gouv.fr https://sentry.incubateur.net https://openmaptiles.geo.data.gouv.fr https://openmaptiles.github.io https://aides-territoires.beta.gouv.fr https://openmaptiles.geo.data.gouv.fr https://wxs.ign.fr https://geo.api.gouv.fr;
   worker-src 'self' blob:;
   font-src 'self' https: data:;
   frame-ancestors 'self' https://matomo.incubateur.anct.gouv.fr;
