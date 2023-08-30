0acc16ebb8f88616005bc6e5dd87e4e3b909de7b
Hugues Maignol <hugues@kime.tech>
2023-08-30 18:24:07 +0200

    fix: do not prefetch backlinks

--- apps/web/src/components/BackLink.tsx
+++ apps/web/src/components/BackLink.tsx
@@ -7,7 +7,7 @@ const BackLink = ({
   href: string
   label?: string
 }) => (
-  <Link href={href} className="fr-link">
+  <Link href={href} prefetch={false} className="fr-link">
     <span className="fr-icon-arrow-left-line fr-icon--sm" /> {label}
   </Link>
 )
