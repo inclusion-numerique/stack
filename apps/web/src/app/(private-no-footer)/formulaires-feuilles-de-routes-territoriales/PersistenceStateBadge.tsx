85a79b25880a9a9074857bb207a3a94bbd6163b7
Hugues Maignol <hugues@kime.tech>
2023-09-06 09:22:16 +0200

    feat: info button on persistence badge

--- apps/web/src/app/(private-no-footer)/formulaires-feuilles-de-routes-territoriales/PersistenceStateBadge.tsx
+++ apps/web/src/app/(private-no-footer)/formulaires-feuilles-de-routes-territoriales/PersistenceStateBadge.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames'
 import React, { PropsWithChildren } from 'react'
+import { createModal } from '@codegouvfr/react-dsfr/Modal'
 import { Spinner } from '@app/web/ui/Spinner'
+import InfoButton from '@app/web/components/InfoButton'
 import styles from './PersistenceStateBadge.module.css'
 
 const PersistenceStateBadgeWrapper = ({ children }: PropsWithChildren) => (
@@ -14,21 +16,36 @@ const PersistenceStateBadgeWrapper = ({ children }: PropsWithChildren) => (
 
 export type PersistenceState = 'saving' | 'saved'
 
-const PersistenceStateBadge = ({ state }: { state: PersistenceState }) => {
-  if (state === 'saving') {
-    return (
+const SavedInformationModal = createModal({
+  id: 'action-bar-saved-information',
+  isOpenedByDefault: false,
+})
+
+const PersistenceStateBadge = ({ state }: { state: PersistenceState }) => (
+  <>
+    {state === 'saving' ? (
       <PersistenceStateBadgeWrapper>
         <Spinner size="small" />
         <span className="fr-ml-1w">Enregistrement&hellip;</span>
       </PersistenceStateBadgeWrapper>
-    )
-  }
-  return (
-    <PersistenceStateBadgeWrapper>
-      <span className="fr-icon--sm fr-mb-0 fr-icon-check-line" />
-      <span className="fr-ml-1w">Enregistré</span>
-    </PersistenceStateBadgeWrapper>
-  )
-}
+    ) : (
+      <PersistenceStateBadgeWrapper>
+        <span className="fr-icon--sm fr-mb-0 fr-icon-check-line" />
+        <span className="fr-ml-1w fr-mr-1v">Enregistré</span>
+        <InfoButton
+          iconId="fr-icon-information-line"
+          title="Informations sur les lieux d'inclusion numérique"
+          onClick={SavedInformationModal.open}
+        />
+      </PersistenceStateBadgeWrapper>
+    )}
+    <SavedInformationModal.Component title="Enregistrement automatique">
+      <p>
+        Votre formulaire est enregistré automatiquement, vous pouvez quitter à
+        tout moment et revenir le compléter plus tard.
+      </p>
+    </SavedInformationModal.Component>
+  </>
+)
 
 export default PersistenceStateBadge
