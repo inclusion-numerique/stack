e8bcdd4fef017eefa0e367e12f541ca97826f2ea
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2023-09-07 18:27:09 +0200

    feat: Add modale on resource view on mobile (#57)
    * feat: Add modale on resource view on mobile
    
    * qa: lint fixes
    
    * fix: border on bottom button
    
    ---------
    
    Co-authored-by: Hugues Maignol <hugues@kime.tech>

--- apps/web/src/components/Resource/View/ResourceNavigation.tsx
+++ apps/web/src/components/Resource/View/ResourceNavigation.tsx
@@ -3,9 +3,18 @@ import { User } from 'next-auth'
 import Link from 'next/link'
 import React from 'react'
 import Button from '@codegouvfr/react-dsfr/Button'
+import { createModal } from '@codegouvfr/react-dsfr/Modal'
 import { Resource } from '@app/web/server/resources/getResource'
 import styles from './ResourceNavigation.module.css'
 import ResourceSideMenu from './ResourceSideMenu'
+
+const {
+  Component: ResourceNavigationModal,
+  buttonProps: resourceNavigationModalNativeButtonProps,
+} = createModal({
+  id: 'resourceNavigation',
+  isOpenedByDefault: false,
+})
 
 const ResourceNavigation = ({
   resource: { slug, contents },
@@ -14,15 +23,24 @@ const ResourceNavigation = ({
   resource: Resource
   user: User | null
 }) => (
-  <div className={styles.container}>
-    <div className={styles.buttons}>
+  <>
+    <ResourceNavigationModal
+      className={styles.modal}
+      title=""
+      buttons={[
+        {
+          title: 'Annuler',
+          priority: 'tertiary',
+          children: 'Annuler',
+        },
+      ]}
+    >
       <Link
         className={classNames(
           'fr-btn',
           'fr-btn--secondary',
           'fr-btn--icon-left',
           'fr-icon-edit-line',
-          styles.button,
         )}
         href={user ? `/ressources/${slug}/editer` : '/'}
       >
@@ -34,25 +52,17 @@ const ResourceNavigation = ({
           'fr-btn--secondary',
           'fr-btn--icon-left',
           'fr-icon-bookmark-line',
-          styles.alwaysVisibleButton,
         )}
         href="/"
       >
         Enregistrer
       </Link>
-      <Button
-        className="fr-hidden-md"
-        title="Plus d'action"
-        priority="tertiary no outline"
-        iconId="fr-icon-more-fill"
-      />
       <Link
         className={classNames(
           'fr-btn',
-          'fr-btn--tertiary',
+          'fr-btn--secondary',
           'fr-btn--icon-left',
           'fr-icon-equalizer-line',
-          styles.button,
         )}
         href="/"
       >
@@ -61,20 +71,79 @@ const ResourceNavigation = ({
       <Link
         className={classNames(
           'fr-btn',
-          'fr-btn--tertiary',
+          'fr-btn--secondary',
           'fr-btn--icon-left',
           'fr-icon-warning-line',
-          styles.button,
         )}
         href="/"
       >
         Signaler
       </Link>
+    </ResourceNavigationModal>
+    <div className={styles.container}>
+      <div className={styles.buttons}>
+        <Link
+          className={classNames(
+            'fr-btn',
+            'fr-btn--secondary',
+            'fr-btn--icon-left',
+            'fr-icon-edit-line',
+            styles.button,
+          )}
+          href={user ? `/ressources/${slug}/editer` : '/'}
+        >
+          {user ? 'Modifier' : 'Demander à contribuer'}
+        </Link>
+        <Link
+          className={classNames(
+            'fr-btn',
+            'fr-btn--secondary',
+            'fr-btn--icon-left',
+            'fr-icon-bookmark-line',
+            styles.alwaysVisibleButton,
+          )}
+          href="/"
+        >
+          Enregistrer
+        </Link>
+        <Button
+          size="large"
+          className="fr-hidden-md"
+          title="Plus d'action"
+          priority="tertiary no outline"
+          iconId="fr-icon-more-fill"
+          {...resourceNavigationModalNativeButtonProps}
+        />
+        <Link
+          className={classNames(
+            'fr-btn',
+            'fr-btn--tertiary',
+            'fr-btn--icon-left',
+            'fr-icon-equalizer-line',
+            styles.button,
+          )}
+          href="/"
+        >
+          Évaluer
+        </Link>
+        <Link
+          className={classNames(
+            'fr-btn',
+            'fr-btn--tertiary',
+            'fr-btn--icon-left',
+            'fr-icon-warning-line',
+            styles.button,
+          )}
+          href="/"
+        >
+          Signaler
+        </Link>
+      </div>
+      <div className="fr-hidden fr-unhidden-md">
+        <ResourceSideMenu contents={contents} />
+      </div>
     </div>
-    <div className="fr-hidden fr-unhidden-md">
-      <ResourceSideMenu contents={contents} />
-    </div>
-  </div>
+  </>
 )
 
 export default ResourceNavigation
