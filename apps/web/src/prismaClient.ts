c816e5852e62b1761c4f3b4fa0d1621961065025
Hugues Maignol <hugues@kime.tech>
2023-09-18 12:35:29 +0200

    qa: debug prisma client

--- apps/web/src/prismaClient.ts
+++ apps/web/src/prismaClient.ts
@@ -1,11 +1,25 @@
 import { PrismaClient } from '@prisma/client'
+import type { Prisma } from '@prisma/client'
 
 // https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
 const globalForPrisma = global as unknown as {
   prismaClient: PrismaClient | undefined
 }
 
-export const prismaClient = globalForPrisma.prismaClient ?? new PrismaClient()
+export const prismaClient =
+  globalForPrisma.prismaClient ??
+  new PrismaClient({
+    log: [
+      {
+        emit: 'event',
+        level: 'query',
+      },
+    ],
+  })
+
+prismaClient.$on('query' as never, (event: Prisma.QueryEvent) => {
+  console.log(`${event.query} ${event.params}`)
+})
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prismaClient = prismaClient
