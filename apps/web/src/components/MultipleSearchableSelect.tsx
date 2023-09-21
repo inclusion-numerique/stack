f56ae525d9032d5228f0c16b4efca0cdb5bb34f8
Desoindx <xavier.desoindre@hotmail.fr>
2023-09-21 14:09:42 +0200

    fix: Refresh issue in multiple seachable select

--- apps/web/src/components/MultipleSearchableSelect.tsx
+++ apps/web/src/components/MultipleSearchableSelect.tsx
@@ -77,7 +77,7 @@ const MultipleSearchableSelect = ({
 
   useEffect(() => {
     setSelecteds([...internalSelecteds])
-  }, [internalSelecteds, setSelecteds])
+  }, [internalSelecteds])
 
   useEffect(() => {
     if (setInput) {
