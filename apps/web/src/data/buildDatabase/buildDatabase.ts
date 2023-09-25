d5b700ad0ed4485b3a5f480a684705ac3e53d01a
Hugues Maignol <hugues@kime.tech>
2023-09-25 18:25:47 +0200

    chore: use interactive transation with large timeout for data load

--- apps/web/src/data/buildDatabase/buildDatabase.ts
+++ apps/web/src/data/buildDatabase/buildDatabase.ts
@@ -1,4 +1,3 @@
-import * as process from 'node:process'
 import { output } from '@app/cli/output'
 import { buildRegions } from '@app/web/data/buildDatabase/buildRegions'
 import { buildDepartements } from '@app/web/data/buildDatabase/buildDepartements'
@@ -12,6 +11,7 @@ import { buildCoordinateursConum } from '@app/web/data/buildDatabase/buildCoordi
 import { buildConumCras } from '@app/web/data/buildDatabase/buildConumCras'
 import { prismaClient } from '@app/web/prismaClient'
 import { getDomainDataForDataIntegrity } from '@app/web/data/buildDatabase/getDomainDataForDataIntegrity'
+import { runPromisesSequentially } from '@app/web/utils/runPromisesSequentially'
 
 export const buildDatabase = async () => {
   output(
@@ -58,124 +58,151 @@ export const buildDatabase = async () => {
 
   const transactionStart = Date.now()
   output('Starting transaction...')
-  console.log('ON', process.env.DATABASE_URL)
   // Executing transaction
-  await prismaClient.$transaction([
-    // Deferring constraints for this transaction
-    prismaClient.$executeRaw`SET CONSTRAINTS ALL DEFERRED;`,
+  await prismaClient.$transaction(
+    async (transaction) => {
+      // Deferring constraints for this transaction
+      await transaction.$executeRaw`SET CONSTRAINTS ALL DEFERRED;`
+      // Avoiding multiple delete queries (first select all then delete where id in) with raw query
+      await transaction.$executeRaw`DELETE
+                                    FROM cra_conseiller_numerique_par_departement`
+      await transaction.$executeRaw`DELETE
+                                    FROM ifn_commune`
+      await transaction.$executeRaw`DELETE
+                                    FROM ifn_epci`
+      await transaction.$executeRaw`DELETE
+                                    FROM coordinateur_conseiller_numerique`
+      await transaction.$executeRaw`DELETE
+                                    FROM conseillers_numeriques_en_permanence`
+      await transaction.$executeRaw`DELETE
+                                    FROM conseiller_numerique`
+      await transaction.$executeRaw`DELETE
+                                    FROM permanences_conseiller_numerique`
+      await transaction.$executeRaw`DELETE
+                                    FROM structures_aidants_connect`
+      await transaction.$executeRaw`DELETE
+                                    FROM structures_cartographie_nationale`
+      await transaction.codesPostaux.deleteMany({
+        where: {
+          OR: [...communes.codePostauxJoinToDelete.values()].map(
+            ({ code, codeCommune }) => ({
+              code,
+              codeCommune,
+            }),
+          ),
+        },
+      })
+      await transaction.codePostal.deleteMany({
+        where: {
+          code: {
+            in: [...communes.codePostalToDelete.values()],
+          },
+        },
+      })
+      await transaction.commune.deleteMany({
+        where: {
+          code: {
+            in: [...communes.communesToDelete.values()],
+          },
+        },
+      })
+      await transaction.epci.deleteMany({
+        where: {
+          code: {
+            in: [...epcis.toDelete.values()],
+          },
+        },
+      })
+      await transaction.departement.deleteMany({
+        where: {
+          code: {
+            in: [...departements.toDelete.values()],
+          },
+        },
+      })
+      await transaction.region.deleteMany({
+        where: {
+          code: {
+            in: [...regions.toDelete.values()],
+          },
+        },
+      })
 
-    // Avoiding multiple delete queries (first select all then delete where id in) with raw query
-    prismaClient.$executeRaw`DELETE FROM cra_conseiller_numerique_par_departement`,
-    prismaClient.$executeRaw`DELETE FROM ifn_commune`,
-    prismaClient.$executeRaw`DELETE FROM ifn_epci`,
-    prismaClient.$executeRaw`DELETE FROM coordinateur_conseiller_numerique`,
-    prismaClient.$executeRaw`DELETE FROM conseillers_numeriques_en_permanence`,
-    prismaClient.$executeRaw`DELETE FROM conseiller_numerique`,
-    prismaClient.$executeRaw`DELETE FROM permanences_conseiller_numerique`,
-    prismaClient.$executeRaw`DELETE FROM structures_aidants_connect`,
-    prismaClient.$executeRaw`DELETE FROM structures_cartographie_nationale`,
-    prismaClient.codesPostaux.deleteMany({
-      where: {
-        OR: [...communes.codePostauxJoinToDelete.values()].map(
-          ({ code, codeCommune }) => ({
-            code,
-            codeCommune,
-          }),
+      // Recreating data from sources
+      await transaction.region.createMany({
+        data: regions.toCreate,
+      })
+
+      await runPromisesSequentially(
+        regions.toUpdate.map((toUpdate) => transaction.region.update(toUpdate)),
+      )
+
+      await transaction.departement.createMany({
+        data: departements.toCreate,
+      })
+
+      await runPromisesSequentially(
+        departements.toUpdate.map((toUpdate) =>
+          transaction.departement.update(toUpdate),
         ),
-      },
-    }),
-    prismaClient.codePostal.deleteMany({
-      where: {
-        code: {
-          in: [...communes.codePostalToDelete.values()],
-        },
-      },
-    }),
-    prismaClient.commune.deleteMany({
-      where: {
-        code: {
-          in: [...communes.communesToDelete.values()],
-        },
-      },
-    }),
-    prismaClient.epci.deleteMany({
-      where: {
-        code: {
-          in: [...epcis.toDelete.values()],
-        },
-      },
-    }),
-    prismaClient.departement.deleteMany({
-      where: {
-        code: {
-          in: [...departements.toDelete.values()],
-        },
-      },
-    }),
-    prismaClient.region.deleteMany({
-      where: {
-        code: {
-          in: [...regions.toDelete.values()],
-        },
-      },
-    }),
+      )
 
-    // Recreating data from sources
-    prismaClient.region.createMany({
-      data: regions.toCreate,
-    }),
-    ...regions.toUpdate.map((toUpdate) => prismaClient.region.update(toUpdate)),
+      await transaction.epci.createMany({
+        data: epcis.toCreate,
+      })
+      await runPromisesSequentially(
+        epcis.toUpdate.map((toUpdate) => prismaClient.epci.update(toUpdate)),
+      )
 
-    prismaClient.departement.createMany({
-      data: departements.toCreate,
-    }),
-    ...departements.toUpdate.map((toUpdate) =>
-      prismaClient.departement.update(toUpdate),
-    ),
+      await transaction.codePostal.createMany({
+        data: communes.codePostalToCreate,
+      })
 
-    prismaClient.epci.createMany({
-      data: epcis.toCreate,
-    }),
-    ...epcis.toUpdate.map((toUpdate) => prismaClient.epci.update(toUpdate)),
+      await transaction.commune.createMany({
+        data: communes.communesToCreate,
+      })
+      await runPromisesSequentially(
+        communes.communesToUpdate.map((toUpdate) =>
+          transaction.commune.update(toUpdate),
+        ),
+      )
 
-    prismaClient.codePostal.createMany({
-      data: communes.codePostalToCreate,
-    }),
-
-    prismaClient.commune.createMany({
-      data: communes.communesToCreate,
-    }),
-    ...communes.communesToUpdate.map((toUpdate) =>
-      prismaClient.commune.update(toUpdate),
-    ),
-
-    prismaClient.codesPostaux.createMany({
-      data: communes.codePostauxJoinToCreate,
-    }),
-    prismaClient.structureCartographieNationale.createMany({
-      data: structuresCartographieNationale.data,
-    }),
-    prismaClient.structureAidantsConnect.createMany({
-      data: structuresAidantsConnect.data,
-    }),
-    prismaClient.conseillerNumerique.createMany({
-      data: permanencesConum.conseillerNumeriqueData,
-    }),
-    prismaClient.coordinateurConseillerNumerique.createMany({
-      data: coordinateursConum.data,
-    }),
-    prismaClient.permanenceConseillerNumerique.createMany({
-      data: permanencesConum.permanenceData,
-    }),
-    prismaClient.conseillerNumeriqueEnPermanence.createMany({
-      data: permanencesConum.conseillersEnPermanenceData,
-    }),
-    prismaClient.ifnEpci.createMany({ data: ifn.ifnEpciData }),
-    prismaClient.ifnCommune.createMany({ data: ifn.ifnCommuneData }),
-    prismaClient.craConseillerNumeriqueParDepartement.createMany({
-      data: conumCras.data,
-    }),
-  ])
+      await transaction.codesPostaux.createMany({
+        data: communes.codePostauxJoinToCreate,
+      })
+      await transaction.structureCartographieNationale.createMany({
+        data: structuresCartographieNationale.data,
+      })
+      await transaction.structureAidantsConnect.createMany({
+        data: structuresAidantsConnect.data,
+      })
+      await transaction.conseillerNumerique.createMany({
+        data: permanencesConum.conseillerNumeriqueData,
+      })
+      await transaction.coordinateurConseillerNumerique.createMany({
+        data: coordinateursConum.data,
+      })
+      await transaction.permanenceConseillerNumerique.createMany({
+        data: permanencesConum.permanenceData,
+      })
+      await transaction.conseillerNumeriqueEnPermanence.createMany({
+        data: permanencesConum.conseillersEnPermanenceData,
+      })
+      await transaction.ifnEpci.createMany({ data: ifn.ifnEpciData })
+      await transaction.ifnCommune.createMany({ data: ifn.ifnCommuneData })
+      await transaction.craConseillerNumeriqueParDepartement.createMany({
+        data: conumCras.data,
+      })
+    },
+    {
+      maxWait: 5000,
+      timeout: 120_000,
+    },
+  )
   output(`Transaction done in ${(Date.now() - transactionStart) / 1000}s`)
+
+  // Requerying data to ensure transaction is ok. It will output the counts
+  await getDomainDataForDataIntegrity()
+
   output('Done')
 }
