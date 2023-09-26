1474e14cc03ed5cb0e33259258f92b49a670c68a
Hugues Maignol <hugues@kime.tech>
2023-09-26 09:29:04 +0200

    chore: debug db url

--- apps/web/src/app/jobs/build-data/route.ts
+++ apps/web/src/app/jobs/build-data/route.ts
@@ -2,6 +2,10 @@ import { buildDatabase } from '@app/web/data/buildDatabase/buildDatabase'
 
 export const POST = async () => {
   const start = new Date()
+
+  // TODO THIS SEEMS TO BE ANOTHER DATABASE THAN THE ONE USED BY THE APP
+  // FIXTURES ARE LOADED TO (RIGHT?) DATABASE FROM CI. COMPARE URLS IN CI AND HERE
+  console.log('BUILD DATA FOR', process.env.DATABASE_URL)
   await buildDatabase()
 
   const end = new Date()
