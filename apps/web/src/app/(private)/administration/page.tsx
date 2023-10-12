8c55dd8e41ca7ce8ce48695e5a00f14203dbab04
Hugues Maignol <hugues@kime.tech>
2023-10-12 12:49:49 +0200

    feat: admin metabase full width

--- apps/web/src/app/(private)/administration/page.tsx
+++ apps/web/src/app/(private)/administration/page.tsx
@@ -15,46 +15,49 @@ const Page = async () => {
   await checkUserAccessToGouvernanceScopeOrNavigate({ national: true })
 
   return (
-    <div
-      className={classNames(
-        'fr-container fr-flex fr-direction-column',
-        styles.pageContainer,
-      )}
-    >
-      <Breadcrumb
-        currentPageLabel="Administration"
-        segments={[
-          {
-            label: "Page d'accueil",
-            linkProps: {
-              href: '/',
+    <>
+      <div
+        className={classNames(
+          'fr-container fr-flex fr-direction-column',
+          styles.pageContainer,
+        )}
+      >
+        <Breadcrumb
+          currentPageLabel="Administration"
+          segments={[
+            {
+              label: "Page d'accueil",
+              linkProps: {
+                href: '/',
+              },
             },
-          },
-        ]}
-      />
-      <h3 className="fr-mb-6v fr-text-title--blue-france">Administration</h3>
-      <WorkInProgressNotice />
-      <div className={styles.metabaseContainer}>
-        <iframe
-          title="Statistiques d’utilisation du service"
-          src="https://metabase.inclusion-numerique.anct.gouv.fr/public/dashboard/38891b8c-dc4e-4e56-a42c-af27865722e4"
-          width="100%"
-          height="100%"
-          style={{
-            overflow: 'hidden',
-            overflowX: 'hidden',
-            overflowY: 'hidden',
-            height: '100%',
-            width: '100%',
-            position: 'absolute',
-            top: '0px',
-            left: '0px',
-            right: '0px',
-            bottom: '0px',
-          }}
+          ]}
         />
+        <h3 className="fr-mb-6v fr-text-title--blue-france">Administration</h3>
       </div>
-    </div>
+      <div className="fr-width-full">
+        <div className={styles.metabaseContainer}>
+          <iframe
+            title="Statistiques d’utilisation du service"
+            src="https://metabase.inclusion-numerique.anct.gouv.fr/public/dashboard/38891b8c-dc4e-4e56-a42c-af27865722e4"
+            width="100%"
+            height="100%"
+            style={{
+              overflow: 'hidden',
+              overflowX: 'hidden',
+              overflowY: 'hidden',
+              height: '100%',
+              width: '100%',
+              position: 'absolute',
+              top: '0px',
+              left: '0px',
+              right: '0px',
+              bottom: '0px',
+            }}
+          />
+        </div>
+      </div>
+    </>
   )
 }
 
