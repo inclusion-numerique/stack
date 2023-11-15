2e7f2b41486284d1ce6ceea03486e88ac2ac4651
Hugues Maignol <hugues@kime.tech>
2023-11-15 14:33:58 +0100

    fix: odt file creation test on ci

--- apps/web/src/server/odt/createOdtFile.spec.ts
+++ apps/web/src/server/odt/createOdtFile.spec.ts
@@ -1,13 +1,16 @@
 import { createOdtFile } from '@app/web/server/odt/createOdtFile'
 import { childrenExample } from '@app/web/server/odt/createOdtContent'
-import { resolve } from 'node:path'
 import { writeFileSync } from 'node:fs'
+import { createVarDirectory } from '@app/config/createVarDirectory'
+import { varDirectory } from '@app/config/varDirectory'
 
 describe('createOdtFile', () => {
-  const variablePath = resolve(__dirname, '../../../../../var')
+  beforeAll(() => {
+    createVarDirectory()
+  })
 
   it('creates a valid odt file', async () => {
     const data = await createOdtFile({ children: childrenExample })
-    writeFileSync(`${variablePath}/test.odt`, data)
+    writeFileSync(`${varDirectory}/test.odt`, data)
   })
 })
