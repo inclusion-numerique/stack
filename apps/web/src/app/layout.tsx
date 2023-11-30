a6f12a29f35e48723a9c7371fe53566cba4317cf
Hugues Maignol <hugues@kime.tech>
2023-11-30 10:29:30 +0100

    feat: use new viewport export

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,7 +3,7 @@
 // setup.ts must be the first import for webpack css chunks to work properly
 // eslint-disable-next-line import/order
 import '@app/web/app/setup'
-import { Metadata } from 'next'
+import { Metadata, Viewport } from 'next'
 import Link from 'next/link'
 import { PropsWithChildren } from 'react'
 import { setLink } from '@codegouvfr/react-dsfr/link'
@@ -26,7 +26,6 @@ setLink({
 
 export const metadata: Metadata = {
   title: PublicWebAppConfig.projectTitle,
-  themeColor: '#000091',
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon/favicon.ico',
@@ -39,6 +38,10 @@ export const metadata: Metadata = {
   },
   description: PublicWebAppConfig.projectTitle,
   manifest: '/favicon/manifest.webmanifest',
+}
+
+export const viewport: Viewport = {
+  themeColor: '#000091',
 }
 
 const RootLayout = ({ children }: PropsWithChildren) => {
