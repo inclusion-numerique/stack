5e5eb973c6f99e605e4b39411df881a86ef4d2c8
Hugues Maignol <hugues@kime.tech>
2023-11-30 17:07:32 +0100

    feat: dynamic health route

--- apps/web/src/app/health/route.ts
+++ apps/web/src/app/health/route.ts
@@ -1,5 +1,8 @@
 import { PublicWebAppConfig } from '@app/web/PublicWebAppConfig'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export const GET = () =>
   new Response(
     JSON.stringify({ status: 'ok', publicConfiguration: PublicWebAppConfig }),
