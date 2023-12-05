170f33610f7ef3c0b0b557536934d971c493afb1
Hugues Maignol <hugues@kime.tech>
2023-12-05 14:38:04 +0100

    feat: Raw Modal

---
+++ packages/ui/src/components/Modal/RawModal.tsx
@@ -0,0 +1,156 @@
+import React, { ReactNode } from 'react'
+import classNames from 'classnames'
+import type { ModalProps } from '@codegouvfr/react-dsfr/Modal'
+import Button from '@codegouvfr/react-dsfr/Button'
+
+/**
+ * This component is used for handling a Modal out of react-dsfr
+ * The react-dsfr Modal has issues with re-renders and bugs with visibility.
+ * This is our own bare-bone implementation of dsfr modal for more control.
+ */
+const RawModal = ({
+  id,
+  title,
+  children,
+  closeTitle = 'Fermer',
+  className,
+  concealingBackdrop = true,
+  iconId,
+  buttons,
+  topAnchor,
+  style,
+  size = 'medium',
+}: {
+  id: string
+  title: ReactNode
+  closeTitle?: string
+  children?: ReactNode
+  className?: string
+} & ModalProps) => {
+  const titleId = `${id}_title`
+
+  // Control button is required for dsfr.js library to work with the modal as a Disclose instance
+  const controlButtonId = `${id}_control_button`
+
+  return (
+    <>
+      <button
+        id={controlButtonId}
+        className="fr-hidden"
+        type="button"
+        data-fr-opened="false"
+        aria-controls={id}
+        tabIndex={-1}
+        aria-hidden
+      />
+      {/* eslint-disable-next-line jsx-a11y/no-redundant-roles */}
+      <dialog
+        role="dialog"
+        aria-labelledby={titleId}
+        id={id}
+        className={classNames(
+          'fr-modal',
+          topAnchor && 'fr-modal--top',
+          className,
+        )}
+        style={style}
+        data-fr-concealing-backdrop={concealingBackdrop}
+        aria-modal="true"
+      >
+        <div className="fr-container fr-container--fluid fr-container-md">
+          <div className="fr-grid-row fr-grid-row--center">
+            <div
+              className={classNames({
+                'fr-col-12 fr-col-md-10 fr-col-lg-8': size === 'large',
+                'fr-col-12 fr-col-md-6 fr-col-lg-4': size === 'small',
+                'fr-col-12 fr-col-md-8 fr-col-lg-6': size === 'medium',
+              })}
+            >
+              <div className="fr-modal__body">
+                <div className="fr-modal__header">
+                  <button
+                    className="fr-btn--close fr-btn"
+                    title={closeTitle}
+                    aria-controls="save-resource-in-collection"
+                    type="button"
+                    data-fr-js-modal-button="true"
+                  >
+                    Fermer
+                  </button>
+                </div>
+                <div className="fr-modal__content">
+                  <h1 id={titleId} className="fr-modal__title">
+                    {!!iconId && (
+                      <span className={classNames(iconId, 'fr-fi--lg')} />
+                    )}
+                    {title}
+                  </h1>
+                  {children}
+                </div>
+                {buttons !== undefined && (
+                  <div className="fr-modal__footer">
+                    <ul
+                      className={classNames(
+                        'fr-btns-group',
+                        'fr-btns-group--right',
+                        'fr-btns-group--inline-reverse',
+                        'fr-btns-group--inline-lg',
+                        'fr-btns-group--icon-left',
+                      )}
+                    >
+                      {(Array.isArray(buttons) ? buttons : [buttons])
+                        .reverse()
+                        .map(
+                          ({ doClosesModal = true, ...buttonProps }, index) => {
+                            if (doClosesModal) {
+                              if (
+                                'linkProps' in buttonProps &&
+                                buttonProps.linkProps
+                              ) {
+                                // eslint-disable-next-line no-param-reassign
+                                buttonProps.linkProps = {
+                                  ...buttonProps.linkProps,
+                                  'aria-controls': id,
+                                }
+                              } else {
+                                // eslint-disable-next-line no-param-reassign
+                                buttonProps.nativeButtonProps = {
+                                  ...buttonProps.nativeButtonProps,
+                                  'aria-controls': id,
+                                }
+                              }
+                            }
+
+                            return (
+                              // eslint-disable-next-line react/no-array-index-key
+                              <li
+                                key={
+                                  buttonProps.nativeButtonProps?.key ??
+                                  buttonProps.linkProps?.key ??
+                                  index
+                                }
+                              >
+                                <Button
+                                  {...buttonProps}
+                                  priority={
+                                    buttonProps.priority ??
+                                    (index === 0 ? 'primary' : 'secondary')
+                                  }
+                                />
+                              </li>
+                            )
+                          },
+                        )}
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </dialog>
+    </>
+  )
+}
+
+export default RawModal
