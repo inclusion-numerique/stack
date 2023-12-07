bfea5ce42ee5685e48502f26e720f5e0d89aeca9
Hugues Maignol <hugues@kime.tech>
2023-12-07 11:12:10 +0100

    ci: fix inclusion connect env vars

--- apps/cli/src/commands/infrastructure/addNextPublicVariablesToDotEnv.ts
+++ apps/cli/src/commands/infrastructure/addNextPublicVariablesToDotEnv.ts
@@ -1,5 +1,6 @@
 import { Argument, Command, Option } from '@commander-js/extra-typings'
 import { appendEnvVariablesToDotEnvFile } from '@app/cli/dotEnvFile'
+import { output } from '@app/cli/output'
 
 export const addNextPublicVariablesToDotEnv = new Command()
   .command('dotenv:add-next-public')
@@ -12,6 +13,8 @@ export const addNextPublicVariablesToDotEnv = new Command()
         ? 'main'
         : 'preview'
 
+    output(`Adding next public variables to .env for "${targetEnv}" target`)
+
     await appendEnvVariablesToDotEnvFile({
       comment: 'Next public environment needed at build time',
       environmentVariables: [
@@ -19,20 +22,12 @@ export const addNextPublicVariablesToDotEnv = new Command()
           ? { name: 'NEXT_PUBLIC_SENTRY_DSN', value: '' }
           : { name: 'NEXT_PUBLIC_SENTRY_ENVIRONMENT', value: namespace },
         {
-          name: 'NEXT_PUBLIC_INCLUSION_CONNECT_ISS',
-          value: `$INCLUSION_CONNECT_${targetEnv.toUpperCase()}_ISS`,
-        },
-        {
           name: 'NEXT_PUBLIC_INCLUSION_CONNECT_HOSTNAME',
           value: `$INCLUSION_CONNECT_${targetEnv.toUpperCase()}_HOSTNAME`,
         },
         {
-          name: 'NEXT_PUBLIC_MONCOMPTEPRO_ISSUER',
-          value: `$MONCOMPTEPRO_${targetEnv.toUpperCase()}_ISSUER`,
-        },
-        {
-          name: 'NEXT_PUBLIC_MONCOMPTEPRO_CLIENT_ID',
-          value: `$MONCOMPTEPRO_${targetEnv.toUpperCase()}_CLIENT_ID`,
+          name: 'NEXT_PUBLIC_INCLUSION_CONNECT_CLIENT_ID',
+          value: `$INCLUSION_CONNECT_${targetEnv.toUpperCase()}_CLIENT_ID`,
         },
       ],
     })
