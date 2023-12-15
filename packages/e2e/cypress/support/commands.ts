5f33c70036501f1abfea3a1157a648d892532bc2
Hugues Maignol <hugues@kime.tech>
2023-12-15 16:30:05 +0100

    feat: add e2e helper commands

--- packages/e2e/cypress/support/commands.ts
+++ packages/e2e/cypress/support/commands.ts
@@ -1,6 +1,6 @@
 import '@testing-library/cypress/add-commands'
+import type { CreateUserInput } from '@app/e2e/e2e/authentication/user.tasks'
 import { appUrl } from '@app/e2e/support/helpers'
-import type { CreateUserInput } from '../e2e/authentication/user.tasks'
 import type { Tasks as CustomTasks } from './tasks'
 import Timeoutable = Cypress.Timeoutable
 import Loggable = Cypress.Loggable
@@ -49,6 +49,13 @@ Cypress.Commands.add('signin', ({ email }: { email: string }) =>
     return cy.wrap(session.sessionToken)
   }),
 )
+
+Cypress.Commands.add('deleteAllData', () => {
+  cy.execute('deleteAllData', {})
+})
+
+Cypress.Commands.add('logout', () => cy.clearCookie('next-auth.session-token'))
+
 Cypress.Commands.add('createUserAndSignin', (user: CreateUserInput) => {
   cy.task('createUser', user)
   return cy.signin(user)
@@ -72,19 +79,82 @@ Cypress.Commands.add('dsfrShouldBeStarted', () => {
 
 Cypress.Commands.add('dsfrModalsShouldBeBound', () => {
   cy.get('dialog.fr-modal').each((modal) => {
+    // TODO There is a regression in the current version of dsfr where this is not sufficient and needs a timeout
     cy.wrap(modal).should('have.attr', 'data-fr-js-modal', 'true')
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(120)
   })
 })
 Cypress.Commands.add('dsfrCollapsesShouldBeBound', () => {
   cy.get('.fr-collapse').each((modal) => {
+    // TODO There is a regression in the current version of dsfr where this is not sufficient and needs a timeout
     cy.wrap(modal).should('have.attr', 'data-fr-js-collapse', 'true')
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(120)
   })
 })
 Cypress.Commands.add('testId', (testId: string) =>
   cy.get(`[data-testid="${testId}"]`),
 )
-Cypress.Commands.add('appUrlShouldBe', (url: string) => {
-  cy.url().should('equal', appUrl(url))
+
+Cypress.Commands.add('getToast', (contains: string | RegExp) =>
+  cy.findAllByRole('status').contains(contains),
+)
+
+// From https://github.com/cypress-io/cypress/issues/877
+Cypress.Commands.add(
+  'positionToViewport',
+  (
+    testId: string,
+    position: 'inside' | 'above' | 'below' | 'left' | 'right',
+  ) => {
+    cy.testId(testId).then(($element) => {
+      const height = Cypress.$(cy.state('window')).height()
+      const width = Cypress.$(cy.state('window')).width()
+      const rect = $element[0].getBoundingClientRect()
+
+      expect(height).to.be.a('number', 'Window height not available')
+      expect(width).to.be.a('number', 'Window width not available')
+      if (!height || !width) {
+        return
+      }
+
+      switch (position) {
+        case 'inside': {
+          expect(rect.top + rect.height / 2).to.be.greaterThan(0)
+          expect(rect.top + rect.height / 2).to.be.lessThan(height)
+          expect(rect.left + rect.width / 2).to.be.greaterThan(0)
+          expect((rect.left, +(rect.width / 2))).to.be.lessThan(width)
+
+          break
+        }
+        case 'above': {
+          expect(rect.top + rect.height / 2).to.be.lessThan(0)
+
+          break
+        }
+        case 'below': {
+          expect(rect.top + rect.height / 2).to.be.greaterThan(height)
+
+          break
+        }
+        case 'left': {
+          expect(rect.left + rect.width / 2).to.be.lessThan(0)
+
+          break
+        }
+        case 'right': {
+          expect(rect.left + rect.width / 2).to.be.greaterThan(width)
+
+          break
+        }
+        default:
+      }
+    })
+  },
+)
+Cypress.Commands.add('appUrlShouldBe', (url: string, options) => {
+  cy.url().should('equal', appUrl(url), options)
 })
 
 //
@@ -103,6 +173,10 @@ declare global {
 
       signin(user: { email: string }): Chainable<string>
 
+      deleteAllData(): Chainable<null>
+
+      logout(): Chainable<null>
+
       dsfrStylesShouldBeLoaded(): Chainable<void>
 
       dsfrShouldBeStarted(): Chainable<void>
@@ -113,7 +187,19 @@ declare global {
 
       testId(testId: string): Chainable<JQuery<HTMLElement>>
 
-      appUrlShouldBe(url: string): Chainable<void>
+      getToast(contains: string | RegExp): Chainable<JQuery<HTMLElement>>
+
+      positionToViewport(
+        testId: string,
+        position: 'inside' | 'above' | 'below' | 'left' | 'right',
+      ): Chainable<void>
+
+      state(type: string): Chainable<unknown>
+
+      appUrlShouldBe(
+        url: string,
+        options?: { timeout?: number },
+      ): Chainable<void>
 
       //       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
       //       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
