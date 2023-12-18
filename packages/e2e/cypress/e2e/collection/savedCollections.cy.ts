c8e5d007bf493ff1a04fecd5b793d905fec1a8be
Hugues Maignol <hugues@kime.tech>
2023-12-18 13:17:07 +0100

    e2e: fix out of viewport click

--- packages/e2e/cypress/e2e/collection/savedCollections.cy.ts
+++ packages/e2e/cypress/e2e/collection/savedCollections.cy.ts
@@ -188,7 +188,10 @@ describe('Utilisateur connecté, je peux ajouter une ressource à une collection
       cy.contains(base.title)
       cy.findAllByRole('button', { name: /enregistrer/i })
         .eq(1)
-        .click()
+        .click({
+          // This is out of viewport in ci so we need to force the click
+          force: true,
+        })
       cy.wait('@saveCollection')
     })
     cy.findByRole('dialog').should('not.exist')
