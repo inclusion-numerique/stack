9f9a77589f1f531c74e8ea2942c3b1633675c5e3
Hugues Maignol <hugues@kime.tech>
2024-01-03 11:39:55 +0100

    fix: allow - and _ in searches

--- apps/web/src/server/search/searchToTsQueryInput.ts
+++ apps/web/src/server/search/searchToTsQueryInput.ts
@@ -4,12 +4,18 @@ export const searchToTsQueryInput = (search?: string | null) => {
     return null
   }
 
-  // Remove all characters that are not letters, digits, or spaces
-  // and include accented characters
-  const cleanedInput = trimmed.replaceAll(/[^\s\wÀ-ÖØ-öø-ÿ]/g, '')
+  const cleanedInput = trimmed
+    // Replace "-" or "_" by spaces
+    .replaceAll(/[_-]/g, ' ')
+    // Remove all characters that are not letters, digits, or spaces
+    // and include accented characters
+    .replaceAll(/[^\s\wÀ-ÖØ-öø-ÿ]/g, '')
 
-  // Split the string into words
-  const words = cleanedInput.split(/\s+/)
+  const words = cleanedInput
+    // Split the string into words
+    .split(/\s+/)
+    // Filter empty words
+    .filter(Boolean)
 
   // Add prefix search for each word
   // Join these words with the '&' operator
