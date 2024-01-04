c37ba6fedc02d5f4aea9b2f8ae044ab8edf8fc00
Thibault <thibault.rouveyrol@gmail.com>
2024-01-04 17:36:06 +0100

    feat: adding fixtures for baseMembers and ressources

--- packages/fixtures/src/resources.ts
+++ packages/fixtures/src/resources.ts
@@ -36,6 +36,40 @@ const resourceBrouillonDansBaseConstant = {
     'Lorem Ipsul Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum quam mauris sit lacinia turpis sed vitae vel. Venenatis in in neque interdum nec facilisi mauris nunc vitae...',
 }
 
+const resourcePubliqueDansProfilConstant = {
+  id: 'd4c3a9c8-01e4-4e46-a3f4-8dab0a3723b2',
+  title:
+    'Ressource publique dans un profil avec un titre sur deux ligne très longues comme comme sur deux lignes',
+  slug: 'ressource-publique-dans-un-profil-avec-un-titre-sur-deux-ligne-tres-longues-comme-comme-sur-deux-lignes',
+  titleDuplicationCheckSlug:
+    'ressource-publique-dans-un-profil-avec-un-titre-sur-deux-ligne-tres-longues-comme-comme-sur-deux-lignes',
+  description:
+    'Lorem Ipsul Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum quam mauris sit lacinia turpis sed vitae vel. Venenatis in in neque interdum nec facilisi mauris nunc vitae...',
+}
+
+const resourcePriveeDansProfilConstant = {
+  id: '8c9e35d2-72d4-4f2f-89b8-d4482e02a42a',
+  title:
+    'Ressource privée dans un profil avec un titre sur deux ligne très longues comme comme sur deux lignes',
+  slug: 'ressource-privee-dans-un-profil-avec-un-titre-sur-deux-ligne-tres-longues-comme-comme-sur-deux-lignes',
+  titleDuplicationCheckSlug:
+    'ressource-privee-dans-un-profil-avec-un-titre-sur-deux-ligne-tres-longues-comme-comme-sur-deux-lignes',
+  description:
+    'Lorem Ipsul Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum quam mauris sit lacinia turpis sed vitae vel. Venenatis in in neque interdum nec facilisi mauris nunc vitae...',
+}
+
+// same for resourceBrouillonDansProfilConstant
+const resourceBrouillonDansProfilConstant = {
+  id: '3f90aecd-7e43-4ac8-9b36-2e4fd71cb6d7',
+  title:
+    'Ressource brouillon dans un profil avec un titre sur deux ligne très longues comme comme sur deux lignes',
+  slug: 'ressource-brouillon-dans-un-profil-avec-un-titre-sur-deux-ligne-tres-longues-comme-comme-sur-deux-lignes',
+  titleDuplicationCheckSlug:
+    'ressource-brouillon-dans-un-profil-avec-un-titre-sur-deux-ligne-tres-longues-comme-comme-sur-deux-lignes',
+  description:
+    'Lorem Ipsul Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bibendum quam mauris sit lacinia turpis sed vitae vel. Venenatis in in neque interdum nec facilisi mauris nunc vitae...',
+}
+
 export const resourcePubliqueDansBase = {
   id: resourcePubliqueDansBaseConstant.id,
   title: resourcePubliqueDansBaseConstant.title,
@@ -168,8 +202,128 @@ export const resourceBrouillonDansBase = {
   },
 } satisfies Prisma.ResourceCreateInput
 
+export const resourcePubliqueDansProfil = {
+  id: resourcePubliqueDansProfilConstant.id,
+  title: resourcePubliqueDansProfilConstant.title,
+  slug: resourcePubliqueDansProfilConstant.slug,
+  titleDuplicationCheckSlug:
+    resourcePubliqueDansProfilConstant.titleDuplicationCheckSlug,
+  description: resourcePubliqueDansProfilConstant.description,
+  createdBy: {
+    connect: {
+      id: jmAvecTout.id,
+    },
+  },
+  isPublic: true,
+
+  events: {
+    connectOrCreate: {
+      where: {
+        id: '4f67a21d-8e6a-4fc3-b947-3156e321f23b',
+      },
+      create: {
+        id: '4f67a21d-8e6a-4fc3-b947-3156e321f23b',
+        byId: jmAvecTout.id,
+        type: 'Created',
+        timestamp: new Date('2021-01-01T00:00:00.000Z'),
+        data: {
+          __version: 1,
+          byId: jmAvecTout.id,
+          title: resourcePubliqueDansProfilConstant.title,
+          description: resourcePubliqueDansProfilConstant.description,
+          slug: resourcePubliqueDansProfilConstant.slug,
+          titleDuplicationCheckSlug:
+            resourcePubliqueDansProfilConstant.titleDuplicationCheckSlug,
+          baseId: null,
+          id: resourcePubliqueDansProfilConstant.id,
+        } satisfies ResourceCreatedDataV1,
+      },
+    },
+  },
+} satisfies Prisma.ResourceCreateInput
+
+export const resourcePriveeDansProfil = {
+  id: resourcePriveeDansProfilConstant.id,
+  title: resourcePriveeDansProfilConstant.title,
+  slug: resourcePriveeDansProfilConstant.slug,
+  titleDuplicationCheckSlug:
+    resourcePriveeDansProfilConstant.titleDuplicationCheckSlug,
+  description: resourcePriveeDansProfilConstant.description,
+  createdBy: {
+    connect: {
+      id: jmAvecTout.id,
+    },
+  },
+  isPublic: true,
+  events: {
+    connectOrCreate: {
+      where: {
+        id: '6a7b8c9d-1e2f-4a8c-bb12-3d8f9a0e5c81',
+      },
+      create: {
+        id: '6a7b8c9d-1e2f-4a8c-bb12-3d8f9a0e5c81',
+        byId: jmAvecTout.id,
+        type: 'Created',
+        timestamp: new Date('2021-01-01T00:00:00.000Z'),
+        data: {
+          __version: 1,
+          byId: jmAvecTout.id,
+          title: resourcePriveeDansProfilConstant.title,
+          description: resourcePriveeDansProfilConstant.description,
+          slug: resourcePriveeDansProfilConstant.slug,
+          titleDuplicationCheckSlug:
+            resourcePriveeDansProfilConstant.titleDuplicationCheckSlug,
+          baseId: null,
+          id: resourcePriveeDansProfilConstant.id,
+        } satisfies ResourceCreatedDataV1,
+      },
+    },
+  },
+} satisfies Prisma.ResourceCreateInput
+
+export const resourceBrouillonDansProfil = {
+  id: resourceBrouillonDansProfilConstant.id,
+  title: resourceBrouillonDansProfilConstant.title,
+  slug: resourceBrouillonDansProfilConstant.slug,
+  titleDuplicationCheckSlug:
+    resourceBrouillonDansProfilConstant.titleDuplicationCheckSlug,
+  description: resourceBrouillonDansProfilConstant.description,
+  createdBy: {
+    connect: {
+      id: jmAvecTout.id,
+    },
+  },
+  events: {
+    connectOrCreate: {
+      where: {
+        id: '7b165f63-4e5b-4c1c-9e7c-3d2df9d73564',
+      },
+      create: {
+        id: '7b165f63-4e5b-4c1c-9e7c-3d2df9d73564',
+        byId: jmAvecTout.id,
+        type: 'Created',
+        timestamp: new Date('2021-01-01T00:00:00.000Z'),
+        data: {
+          __version: 1,
+          byId: jmAvecTout.id,
+          title: resourceBrouillonDansProfilConstant.title,
+          description: resourceBrouillonDansProfilConstant.description,
+          slug: resourceBrouillonDansProfilConstant.slug,
+          titleDuplicationCheckSlug:
+            resourceBrouillonDansProfilConstant.titleDuplicationCheckSlug,
+          baseId: null,
+          id: resourceBrouillonDansProfilConstant.id,
+        } satisfies ResourceCreatedDataV1,
+      },
+    },
+  },
+} satisfies Prisma.ResourceCreateInput
+
 export const resources = [
   resourcePubliqueDansBase,
   resourcePriveeDansBase,
   resourceBrouillonDansBase,
+  resourcePubliqueDansProfil,
+  resourcePriveeDansProfil,
+  resourceBrouillonDansProfil,
 ]
