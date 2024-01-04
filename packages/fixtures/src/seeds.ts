59335e0234f6e4400d80d646265c027f57470913
Hugues Maignol <hugues@kime.tech>
2024-01-04 15:31:36 +0100

    feat: user seeds

--- packages/fixtures/src/seeds.ts
+++ packages/fixtures/src/seeds.ts
@@ -1,15 +1,11 @@
 import { Command, InvalidArgumentError } from '@commander-js/extra-typings'
 import { prismaClient } from '@app/web/prismaClient'
-import { createResourceProjection } from '@app/web/server/resources/feature/createResourceProjection'
 import { Prisma } from '@prisma/client'
 import { faker } from '@faker-js/faker'
-import { bases, randomBases } from './bases'
-import { randomResourcesEvents, resources } from './resources'
-import { randomUsers, users } from './users'
-import { randomMembers } from './members'
-import { randomContributors } from './contributors'
+import { bases } from './bases'
+import { resources } from './resources'
+import { users } from './users'
 
-import { randomCollections, randomResourcesInCollections } from './collections'
 import TransactionClient = Prisma.TransactionClient
 
 function myParseInt(value: string) {
@@ -40,106 +36,49 @@ const deleteAll = async (transaction: TransactionClient) => {
 }
 
 const seed = async (transaction: TransactionClient, random?: number) => {
-  if (random) {
-    await transaction.user.createMany({ data: randomUsers(random) })
-    const newBases = await randomBases(transaction, random)
-    await transaction.base.createMany({ data: newBases })
-    const members = await randomMembers(transaction)
-    await transaction.baseMembers.createMany({ data: members })
-    const collections = await randomCollections(transaction, random)
-    await transaction.collection.createMany({ data: collections })
-
-    const newResourcesEvents = await randomResourcesEvents(transaction, random)
-    const projections = newResourcesEvents.map(createResourceProjection)
-    await transaction.resource.createMany({
-      data: projections.map((projection) => ({
-        ...projection,
-        isPublic: true,
-        titleDuplicationCheckSlug: projection.slug,
-        contents: undefined,
-        contributors: undefined,
-      })),
-    })
-
-    await transaction.content.createMany({
-      data: projections.flatMap((projection) =>
-        projection.contents.map((content, index) => ({
-          ...content,
-          order: index,
-          resourceId: projection.id,
-          __version: undefined,
-        })),
-      ),
-    })
-
-    await transaction.resourceEvent.createMany({
-      data: newResourcesEvents.flat(),
-    })
-
-    const contributors = await randomContributors(transaction)
-    await transaction.resourceContributors.createMany({ data: contributors })
-
-    const resourcesInCollections =
-      await randomResourcesInCollections(transaction)
-    await Promise.all(
-      resourcesInCollections.map(({ resourceId, collectionId }) =>
-        transaction.resource.update({
-          data: {
-            collections: {
-              create: {
-                collectionId,
-              },
-            },
-          },
-          where: { id: resourceId },
-        }),
-      ),
-    )
-  } else {
-    await Promise.all(
-      users.map((user) =>
-        transaction.user.upsert({
-          where: { id: user.id },
-          create: user,
-          update: user,
-        }),
-      ),
-    )
-    await Promise.all(
-      users.map((user) =>
-        transaction.collection.create({
-          data: {
-            id: faker.string.uuid(),
-            title: 'Mes favoris',
-            ownerId: user.id as string,
-            isPublic: false,
-            isFavorites: true,
-          },
-        }),
-      ),
-    )
-    await Promise.all(
-      bases.map((base) =>
-        transaction.base.upsert({
-          where: { id: base.id },
-          create: base,
-          update: base,
-        }),
-      ),
-    )
-    await Promise.all(
-      resources.map((resource) =>
-        transaction.resource.upsert({
-          where: { id: resource.id },
-          create: resource,
-          update: resource,
-        }),
-      ),
-    )
-  }
+  await Promise.all(
+    users.map((user) =>
+      transaction.user.upsert({
+        where: { id: user.id },
+        create: user,
+        update: user,
+      }),
+    ),
+  )
+  await Promise.all(
+    users.map((user) =>
+      transaction.collection.create({
+        data: {
+          id: faker.string.uuid(),
+          title: 'Mes favoris',
+          ownerId: user.id as string,
+          isPublic: false,
+          isFavorites: true,
+        },
+      }),
+    ),
+  )
+  await Promise.all(
+    bases.map((base) =>
+      transaction.base.upsert({
+        where: { id: base.id },
+        create: base,
+        update: base,
+      }),
+    ),
+  )
+  await Promise.all(
+    resources.map((resource) =>
+      transaction.resource.upsert({
+        where: { id: resource.id },
+        create: resource,
+        update: resource,
+      }),
+    ),
+  )
 }
 
-const main = async (eraseAllData: boolean, random?: number) => {
+const main = async (eraseAllData: boolean) => {
   await prismaClient.$transaction(
     async (transaction) => {
       if (eraseAllData) {
@@ -147,12 +86,8 @@ const main = async (eraseAllData: boolean, random?: number) => {
         await deleteAll(transaction)
       }
 
-      console.log(
-        `Generating ${
-          random ? `${random} set of random` : 'non-random fixtures'
-        } data`,
-      )
-      await seed(transaction, random)
+      console.log(`Generating fixtures data`)
+      await seed(transaction)
     },
     { maxWait: 60_000 },
   )
@@ -170,9 +105,9 @@ const program = new Command()
 
 program.parse()
 
-const { eraseAllData, random } = program.opts()
+const { eraseAllData } = program.opts()
 
-main(eraseAllData, random === true ? 1 : random)
+main(eraseAllData)
   // eslint-disable-next-line promise/always-return
   .then(() => prismaClient.$disconnect())
   // eslint-disable-next-line unicorn/prefer-top-level-await
--- packages/fixtures/src/users.ts
+++ packages/fixtures/src/users.ts
@@ -1,52 +1,30 @@
-import { faker } from '@faker-js/faker'
-import { Prisma } from '@prisma/client'
-import { prismaClient } from '@app/web/prismaClient'
+import type { Prisma } from '@prisma/client'
 
-const BASE_NUMBER = 100
+export const edith = {
+  id: '99afd613-9d54-4110-9062-065c627eda8a',
+  firstName: 'Edith',
+  lastName: 'Piaf',
+  name: 'Edith Piaf',
+  email: 'edith@piaf.com',
+  emailVerified: new Date(),
+  isPublic: true,
+} satisfies Prisma.UserCreateManyInput
 
-export const users: Prisma.UserCreateManyInput[] = [
-  {
-    id: '99afd613-9d54-4110-9062-065c627eda8a',
-    firstName: 'Edith',
-    lastName: 'Piaf',
-    name: 'Edith Piaf',
-    email: 'edith@piaf.com',
-    emailVerified: new Date(),
-    isPublic: true,
-  },
-  {
-    id: 'f1826416-af31-402c-9d92-379d4ea7509e',
-    firstName: 'Joe',
-    lastName: 'Dassin',
-    name: 'Joe Dassin',
-    email: 'joe@dassin.com',
-    emailVerified: new Date(),
-  },
-  {
-    id: 'eecac657-f415-47e1-8087-c4508ea16191',
-    firstName: 'Georges',
-    lastName: 'Moustaki',
-    name: 'Georges Moustaki',
-    email: 'georges@moustaki.com',
-  },
-]
+export const joe = {
+  id: 'f1826416-af31-402c-9d92-379d4ea7509e',
+  firstName: 'Joe',
+  lastName: 'Dassin',
+  name: 'Joe Dassin',
+  email: 'joe@dassin.com',
+  emailVerified: new Date(),
+} satisfies Prisma.UserCreateManyInput
 
-export const randomUsers: (
-  random: number,
-) => Exclude<
-  Parameters<typeof prismaClient.user.create>[0]['data'],
-  undefined
->[] = (random) =>
-  Array.from({ length: random * BASE_NUMBER }, (_, index) => {
-    const firstName = faker.person.firstName()
-    const lastName = faker.person.lastName()
+export const georges = {
+  id: 'eecac657-f415-47e1-8087-c4508ea16191',
+  firstName: 'Georges',
+  lastName: 'Moustaki',
+  name: 'Georges Moustaki',
+  email: 'georges@moustaki.com',
+} satisfies Prisma.UserCreateManyInput
 
-    return {
-      firstName,
-      lastName,
-      name: `${firstName} ${lastName}`,
-      email: faker.internet.email().toLowerCase(),
-      emailVerified: index % 3 ? null : new Date(),
-      isPublic: Math.random() > 0.1,
-    }
-  })
+export const users = [edith, joe, georges]
