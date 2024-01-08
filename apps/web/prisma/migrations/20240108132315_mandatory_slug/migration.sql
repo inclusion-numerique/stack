eac965f375a0a4230e320bbccdd105f919c2d3e1
Hugues Maignol <hugues@kime.tech>
2024-01-08 16:36:26 +0100

    fix: collection slug collision

--- apps/web/prisma/migrations/20240108132315_mandatory_slug/migration.sql
+++ apps/web/prisma/migrations/20240108132315_mandatory_slug/migration.sql
@@ -12,16 +12,20 @@ WHERE "users"."id" = numbered_users."id";
 WITH users_slug AS (
     SELECT "id", "slug"
     FROM "users"
+),
+numbered_collections AS (
+ SELECT "id", "title", "is_favorites", "owner_id", ROW_NUMBER() OVER (ORDER BY "id") AS rn
+ FROM "collections"
 )
 UPDATE "collections"
 SET "slug" =
         CASE
-            WHEN "collections"."is_favorites" THEN users_slug."slug" || '-' || LOWER(REGEXP_REPLACE("title", '[^a-zA-Z0-9]+', '-', 'g')) || '-' || LEFT("collections"."id"::text, 2)
-            WHEN "collections"."title" = 'Ressources enregistrées' THEN users_slug."slug" || '-ressources-enregistrees-' || LEFT("collections"."id"::text, 2)
-            ELSE LOWER(REGEXP_REPLACE("title", '[^a-zA-Z0-9]+', '-', 'g')) || '-' || LEFT("collections"."id"::text, 2)
+            WHEN numbered_collections."is_favorites" THEN users_slug."slug" || '-' || LOWER(REGEXP_REPLACE("title", '[^a-zA-Z0-9]+', '-', 'g')) || '-' || numbered_collections.rn
+            WHEN numbered_collections."title" = 'Ressources enregistrées' THEN users_slug."slug" || '-ressources-enregistrees-' || numbered_collections.rn
+            ELSE LOWER(REGEXP_REPLACE("title", '[^a-zA-Z0-9]+', '-', 'g')) || '-' || numbered_collections.rn
             END
-FROM users_slug
-WHERE "collections"."owner_id" = users_slug."id";
+FROM users_slug, numbered_collections
+WHERE "collections"."id" = numbered_collections."id" AND "collections"."owner_id" = users_slug."id";
 
 
 -- AlterTable
