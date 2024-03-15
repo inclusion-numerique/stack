a1aa2b95ffc930ca4731b5f111a3f5d858ae2a82
Hugues Maignol <hugues@kime.tech>
2024-03-15 10:57:11 +0700

    feat: mutiple select form field improved with optgroups

--- apps/web/src/utils/options.ts
+++ apps/web/src/utils/options.ts
@@ -1,10 +1,19 @@
+/**
+ * @deprecated Import from @app/ui/components/Form/utils/options instead
+ */
 export type Option<T extends string = string> = {
   name: string
   value: T
   disabled?: boolean
 }
+/**
+ * @deprecated Import from @app/ui/components/Form/utils/options instead
+ */
 export type Options<T extends string = string> = Option<T>[]
 
+/**
+ * @deprecated Import from @app/ui/components/Form/utils/options instead
+ */
 export const labelsToOptions = <T extends string>(
   object: Record<T, string>,
 ): Options<T> =>
