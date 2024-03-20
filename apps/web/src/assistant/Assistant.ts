ef0bf5ed8d9559d65f6cf17ebbd35c88c40eaa07
Thibault <thibault.rouveyrol@gmail.com>
2024-03-20 16:24:31 +0100

    qa: fix lints

--- apps/web/src/assistant/Assistant.ts
+++ apps/web/src/assistant/Assistant.ts
@@ -1,4 +1,24 @@
 import z from 'zod'
+// import MistralClient from '@mistralai/mistralai'
+// import { ServerWebAppConfig } from '@app/web/ServerWebAppConfig'
+
+// const { apiKey } = ServerWebAppConfig.Mistral
+
+// const client = new MistralClient(apiKey)
+
+// export const generateEmbeddings = async (texts: []) => {
+//   const input = []
+//   for (let index = 0; index < 1; index++) {
+//     input.push('What is the best French cheese?')
+//   }
+//
+//   const embeddingsBatchResponse = await client.embeddings({
+//     model: 'mistral-embed',
+//     input,
+//   })
+//
+//   console.log('Embeddings Batch:', embeddingsBatchResponse.data)
+// }
 
 export const AssistantValidation = z.object({
   prompt: z.string().trim(),
