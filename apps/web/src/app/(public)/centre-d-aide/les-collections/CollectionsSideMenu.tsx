1240282f6a34d0d6d92a0abb75a774d3ce149aff
Marc Gavanier <marc.gavanier@gmail.com>
2024-03-28 16:49:44 +0100

    fix: centre d'aide pages design

--- apps/web/src/app/(public)/centre-d-aide/les-collections/CollectionsSideMenu.tsx
+++ apps/web/src/app/(public)/centre-d-aide/les-collections/CollectionsSideMenu.tsx
@@ -9,9 +9,15 @@ const COLLECTIONS_SECTIONS = [
     title: 'Une collection, c’est quoi ?',
   },
   { id: 'creation-collection', title: 'Création d’une collection' },
-  { id: 'ressources-collection', title: 'Enregistrer des ressources dans une collection' },
+  {
+    id: 'ressources-collection',
+    title: 'Enregistrer des ressources dans une collection',
+  },
   { id: 'enregistrer-collection', title: 'Enregistrer une collection' },
-  { id: 'infos-collection', title: 'Informations & paramètres d’une collection' },
+  {
+    id: 'infos-collection',
+    title: 'Informations & paramètres d’une collection',
+  },
 ]
 
 const CollectionsSideMenu = () => (
