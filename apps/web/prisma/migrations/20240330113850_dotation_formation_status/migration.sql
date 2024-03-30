9b44e89ce315a8b3912245c5e025456030cc0b73
Hugues Maignol <hugues@kime.tech>
2024-03-30 19:29:18 +0700

    feat: beneficiaire dotation formation statut migration

---
+++ apps/web/prisma/migrations/20240330113850_dotation_formation_status/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "gouvernances" ADD COLUMN     "beneficiaire_dotation_formation_demande_de_modification" TIMESTAMP(3),
+ADD COLUMN     "beneficiaire_dotation_formation_valide" TIMESTAMP(3);
