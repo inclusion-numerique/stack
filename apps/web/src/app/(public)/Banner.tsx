1ae26e767603269d853a0d1fe6fd33648d139efe
Thibault <thibault.rouveyrol@gmail.com>
2024-04-05 11:46:40 +0200

    feat: remove Notice from landing

--- apps/web/src/app/(public)/Banner.tsx
+++ apps/web/src/app/(public)/Banner.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Notice from '@codegouvfr/react-dsfr/Notice'
-import Button from '@codegouvfr/react-dsfr/Button'
 import Link from 'next/link'
 import SearchBar from '@app/web/components/Search/SearchBar'
 import styles from './Banner.module.css'
@@ -13,23 +11,6 @@ const Banner = () => (
         La plateforme collaborative de partage de ressources & communs
         numériques à l’échelle nationale.
       </p>
-      <Notice
-        className="fr-notice--new fr-my-10v fr-mx-auto"
-        style={{ maxWidth: 640 }}
-        title={
-          <span className="fr-flex fr-justify-content-space-between fr-align-items-center fr-direction-column fr-direction-md-row fr-flex-gap-1v">
-            <span>Bienvenue sur la nouvelle version&nbsp;!</span>
-            <Button
-              iconId="fr-icon-arrow-right-line"
-              priority="tertiary no outline"
-              linkProps={{ href: '/nouveautes' }}
-              iconPosition="right"
-            >
-              Découvrir les nouveautés
-            </Button>
-          </span>
-        }
-      />
       <SearchBar />
       <div className="fr-my-4v">
         <Link href="/rechercher/tout/ressources" className="fr-link">
