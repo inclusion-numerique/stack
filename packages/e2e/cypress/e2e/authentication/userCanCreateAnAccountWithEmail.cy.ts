4300c231766404f3c7b20726cea39c575eb170d5
Hugues Maignol <hugues@kime.tech>
2024-04-08 10:31:04 +0200

    chore: fix ci e2e network error on maildev

--- packages/e2e/cypress/e2e/authentication/userCanCreateAnAccountWithEmail.cy.ts
+++ packages/e2e/cypress/e2e/authentication/userCanCreateAnAccountWithEmail.cy.ts
@@ -1,4 +1,5 @@
 import { appUrl, createTestUser } from '../../support/helpers'
+import { goToMostRecentEmailReceived } from '@app/e2e/e2e/goToMostRecentEmailReceived'
 
 describe('ETQ Utilisateur, lorsque je clique sur “Se créer un compte”, je peux créer un compte avec mon mail', () => {
   /**
@@ -25,21 +26,8 @@ describe('ETQ Utilisateur, lorsque je clique sur “Se créer un compte”, je p
     cy.contains('Un lien de connexion sécurisé a été envoyé')
     cy.contains(email)
 
-    cy.log('Go check emails in maildev server')
-    // Go to maildev server to checkout the email and get the magic link
-    cy.visit('localhost:1080')
-    cy.get('.email-list li a').first().click()
-
-    cy.get('.email-meta .subject').should('contain', 'Connexion à Stack')
-
-    // Cypress does not work well with iframes, we go to the html source of the email that is
-    // included in the iframe preview of maildev ui
-    cy.url().then((url) => {
-      const emailPath = url.split('#').at(-1)
-      if (!emailPath) {
-        throw new Error('Could not find email content path from maildev url')
-      }
-      cy.visit(`localhost:1080${emailPath}/html`)
+    goToMostRecentEmailReceived({
+      subjectInclude: 'Connexion à Stack',
     })
 
     cy.log('Check mail contents')
