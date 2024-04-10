24c8e1ae32d751ea011211f2f89891f745215e9e
Hugues Maignol <hugues@kime.tech>
2024-04-10 19:20:05 +0200

    qa: lint fix

--- packages/e2e/cypress/e2e/home.cy.ts
+++ packages/e2e/cypress/e2e/home.cy.ts
@@ -1,10 +1,13 @@
 import { createTestUser } from '../support/helpers'
 
-describe('Page d\'accueil', () => {
-  it('La page d\'accueil s\'affiche correctement, avec les styles du DSFR', () => {
+describe('Page d’accueil', () => {
+  it('La page d’accueil s’affiche correctement, avec les styles du DSFR', () => {
     cy.visit('/')
     cy.dsfrStylesShouldBeLoaded()
-    cy.get('.fr-header__service a').should('contain', 'La coop de la médiation numérique')
+    cy.get('.fr-header__service a').should(
+      'contain',
+      'La coop de la médiation numérique',
+    )
     cy.get('.fr-header__tools .fr-btn')
       .should('contain', 'Se connecter')
       .should('have.css', 'color', 'rgb(0, 0, 145)')
@@ -16,7 +19,7 @@ describe('Page d\'accueil', () => {
     cy.get('.fr-header').should('have.css', 'filter', 'none')
   })
 
-  it('La page d\'accueil affiche le statut de connexion de l\'utilisateur', () => {
+  it('La page d’accueil affiche le statut de connexion de l’utilisateur', () => {
     const user = createTestUser()
     cy.createUserAndSignin(user)
     cy.visit('/')
