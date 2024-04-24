3ec9383e84f05d702b1e343184e5a9b2cbbacf29
Hugues Maignol <hugues@kime.tech>
2024-04-24 10:16:10 +0200

    feat: security txt

---
+++ apps/web/src/app/.well-known/security.txt/route.ts
@@ -0,0 +1,5 @@
+export const GET = () =>
+  new Response(`Contact: mailto:support-incubateur@anct.gouv.fr
+Policy: https://incubateur.anct.gouv.fr/security-policy
+Preferred-Languages: fr, en
+Expires: 2024-12-31T00:00:00.000Z`)
