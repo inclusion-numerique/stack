70d0dd0012b786b026f8b3682ce9eaa399f344ca
Hugues Maignol <hugues@kime.tech>
2024-04-25 17:48:44 +0200

    fix: component testing compilation

--- packages/e2e/cypress/components/web/components/Header.cy.tsx
+++ packages/e2e/cypress/components/web/components/Header.cy.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Header from '@app/web/components/Header'
+// import React from 'react'
+// import Header from '@app/web/components/Header'
 
 /**
  * While cypress components do not support server actions, we cannot test Header
@@ -8,14 +8,14 @@ import Header from '@app/web/components/Header'
 describe.skip('<Header />', () => {
   it('Should display desktop header without dropshadow', () => {
     cy.viewport(1200, 750) // Set viewport to 550px x 750px
-    cy.mount(<Header />)
+    // cy.mount(<Header />)
     cy.get('.fr-header').should('have.css', 'filter', 'none')
     cy.get('.fr-header__brand').should('have.css', 'filter', 'none')
   })
 
   it('Should display mobile header without dropshadow', () => {
     cy.viewport(500, 500) // Set viewport to 550px x 750px
-    cy.mount(<Header />)
+    // cy.mount(<Header />)
     cy.get('.fr-header').should('have.css', 'filter', 'none')
     cy.get('.fr-header__brand').should('have.css', 'filter', 'none')
   })
