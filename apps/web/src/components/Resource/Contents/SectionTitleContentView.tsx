0ade44f9c309a9e15583f769a374dc4814430fe7
Thibault <thibault.rouveyrol@gmail.com>
2024-05-15 14:43:56 +0200

    fix: section title and File title font size

--- apps/web/src/components/Resource/Contents/SectionTitleContentView.tsx
+++ apps/web/src/components/Resource/Contents/SectionTitleContentView.tsx
@@ -6,7 +6,7 @@ const SectionTitleContentView = ({
   content: Pick<ResourceContent, 'title'>
 }) =>
   title ? (
-    <h2 data-testid="content-section-title" className="fr-mb-0 fr-mt-4v">
+    <h2 data-testid="content-section-title" className="fr-mt-4v fr-h4 ">
       {title}
     </h2>
   ) : null
