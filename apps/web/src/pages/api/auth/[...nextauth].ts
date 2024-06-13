a88bcf87aff7e1dd342c45f33a2098c4c85c255b
Hugues Maignol <hugues@kime.tech>
2024-06-13 18:14:10 +0200

    fix: firewall user agents

--- apps/web/src/pages/api/auth/[...nextauth].ts
+++ apps/web/src/pages/api/auth/[...nextauth].ts
@@ -6,6 +6,7 @@ import '@app/web/auth/nextAuthSetup'
 import { sendVerificationRequest } from '@app/web/auth/sendVerificationRequest'
 import { ServerWebAppConfig } from '@app/web/ServerWebAppConfig'
 import { InclusionConnectProvider } from '@app/web/auth/InclusionConnectProvider'
+import { isFirewallUserAgent } from '@app/web/pages/api/auth/isFirewallUserAgent'
 
 export const authOptions: NextAuthOptions = {
   // debug: process.env.NODE_ENV !== 'production',
@@ -59,8 +60,8 @@ export const authOptions: NextAuthOptions = {
 
 export default function auth(request: NextApiRequest, res: NextApiResponse) {
   // https://next-auth.js.org/tutorials/avoid-corporate-link-checking-email-provider
-  if (request.method === 'HEAD') {
-    return res.status(200).end()
+  if (isFirewallUserAgent(request)) {
+    return res.status(400).end()
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
---
+++ apps/web/src/pages/api/auth/isFirewallUserAgent.ts
@@ -0,0 +1,81 @@
+import type { NextApiRequest } from 'next'
+
+export const isFirewallUserAgent = (request: NextApiRequest): boolean => {
+  const userAgent = request.headers['user-agent'] || ''
+
+  // Liste étendue des signatures connues des agents utilisateurs de pare-feu
+  const firewallUserAgents = [
+    /Bitdefender/,
+    /Kaspersky/,
+    /Norton/,
+    /McAfee/,
+    /Trend Micro/,
+    /Sophos/,
+    /F-Secure/,
+    /Avast/,
+    /Avira/,
+    /Comodo/,
+    /Stormshield/,
+    /Fortinet/,
+    /FortiGate/,
+    /Palo Alto/,
+    /Cisco/,
+    /Check Point/,
+    /Juniper/,
+    /Symantec/,
+    /Webroot/,
+    /ESET/,
+    /Barracuda/,
+    /SonicWall/,
+    /FireEye/,
+    /WatchGuard/,
+    /Cyberoam/,
+    /Dr.Web/,
+    /Panda Security/,
+  ]
+
+  // Liste étendue des signatures connues des clients de messagerie
+  const mailClientUserAgents = [
+    /Outlook/,
+    /Microsoft Outlook/,
+    /Microsoft Office/,
+    /Windows NT [\d.]+; WOW64; Microsoft Outlook/,
+    /MSOffice/,
+    /Thunderbird/,
+    /Apple Mail/,
+    /Mail/,
+    /Gmail/,
+    /YahooMail/,
+    /YahooMobile/,
+    /Mailbird/,
+    /eM Client/,
+    /Zoho Mail/,
+    /Airmail/,
+    /Postbox/,
+    /Spark/,
+    /Mailspring/,
+    /The Bat!/,
+    /Foxmail/,
+    /BlueMail/,
+    /Claws Mail/,
+    /SeaMonkey/,
+    /IncrediMail/,
+    /K-9 Mail/,
+    /AquaMail/,
+    /ProtonMail/,
+    /Tutanota/,
+    /Newton Mail/,
+    /Canary Mail/,
+    /FastMail/,
+  ]
+
+  // Vérification des agents utilisateurs suspects
+  const isFirewall = firewallUserAgents.some((pattern) =>
+    pattern.test(userAgent),
+  )
+  const isMailClient = mailClientUserAgents.some((pattern) =>
+    pattern.test(userAgent),
+  )
+
+  return isFirewall || isMailClient
+}
