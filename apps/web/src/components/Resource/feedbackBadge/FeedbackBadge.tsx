bedd1661f64c919ee6f46dbb0ca892ba1618668f
Marc Gavanier <marc.gavanier@gmail.com>
2024-06-14 11:11:15 +0200

    fix: resource card and view design

--- apps/web/src/components/Resource/feedbackBadge/FeedbackBadge.tsx
+++ apps/web/src/components/Resource/feedbackBadge/FeedbackBadge.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import React from 'react'
 import Badge from '@codegouvfr/react-dsfr/Badge'
 import type { AlertProps } from '@codegouvfr/react-dsfr/src/Alert'
@@ -6,9 +7,11 @@ import { feedbackBadgeStatus } from './feedbackBadge.Status'
 export const FeedbackBadge = ({
   value,
   customThresholds,
+  className,
 }: {
   value: number
   customThresholds?: [number, number, number, number]
+  className?: string
 }) => {
   const badgeStatus = feedbackBadgeStatus(value, customThresholds)
 
@@ -17,7 +20,10 @@ export const FeedbackBadge = ({
       noIcon
       small
       severity={badgeStatus?.severity as AlertProps.Severity}
-      className="fr-pl-1v fr-text--center fr-text--transform-uppercase fr-text--nowrap"
+      className={classNames(
+        'fr-pl-1v fr-text--center fr-text--transform-uppercase fr-text--nowrap',
+        className,
+      )}
     >
       <span className={`${badgeStatus?.emoticon} ri-lg fr-mr-1v`} aria-hidden />
       {badgeStatus?.label}
