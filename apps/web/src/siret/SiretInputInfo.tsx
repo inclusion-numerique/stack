ebf14a47e45ec79d8bbc492e7208572af022cbe8
Hugues Maignol <hugues@kime.tech>
2024-06-20 15:20:35 +0200

    feat: auto completable siret input info

--- apps/web/src/siret/SiretInputInfo.tsx
+++ apps/web/src/siret/SiretInputInfo.tsx
@@ -2,9 +2,15 @@ import classNames from 'classnames'
 import Link from 'next/link'
 import React from 'react'
 
-const SiretInputInfo = ({ className }: { className?: string }) => (
+const SiretInputInfo = ({
+  className,
+  searchTerm,
+}: {
+  className?: string
+  searchTerm?: string
+}) => (
   <Link
-    href="https://annuaire-entreprises.data.gouv.fr"
+    href={`https://annuaire-entreprises.data.gouv.fr${searchTerm ? `/rechercher?terme=${searchTerm}` : ''}`}
     target="_blank"
     className={classNames(
       'fr-mt-2v fr-display-inline-block fr-text-action-high--blue-france fr-text--sm',
