8346fc6e699bdf51a0c9b4a8b87d4bd795b9fb9e
Hugues Maignol <hugues@kime.tech>
2024-07-18 09:56:20 +0200

    qa: lint fix

--- apps/web/src/app/coop/mon-activite/cra/administratif/CraDemarcheAdministrativeForm.tsx
+++ apps/web/src/app/coop/mon-activite/cra/administratif/CraDemarcheAdministrativeForm.tsx
@@ -1,6 +1,13 @@
 'use client'
 
-import { Control, DefaultValues, useForm, UseFormGetValues, UseFormSetValue, UseFormWatch } from 'react-hook-form'
+import {
+  Control,
+  DefaultValues,
+  useForm,
+  UseFormGetValues,
+  UseFormSetValue,
+  UseFormWatch,
+} from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import CheckboxGroupFormField from '@app/ui/components/Form/CheckboxGroupFormField'
 import RedAsterisk from '@app/ui/components/Form/RedAsterisk'
@@ -16,8 +23,14 @@ import React, { useState } from 'react'
 import type { SelectOption } from '@app/ui/components/Form/utils/options'
 import { useScrollToError } from '@app/ui/hooks/useScrollToError'
 import CraFormLabel from '@app/web/app/coop/mon-activite/cra/CraFormLabel'
-import AdresseBanFormField, { type AdressBanFormFieldOption } from '@app/web/components/form/AdresseBanFormField'
-import { genreOptions, statutSocialOptions, trancheAgeOptions } from '@app/web/beneficiaire/beneficiaire'
+import AdresseBanFormField, {
+  type AdressBanFormFieldOption,
+} from '@app/web/components/form/AdresseBanFormField'
+import {
+  genreOptions,
+  statutSocialOptions,
+  trancheAgeOptions,
+} from '@app/web/beneficiaire/beneficiaire'
 import { applyZodValidationMutationErrorsToForm } from '@app/web/utils/applyZodValidationMutationErrorsToForm'
 import { trpc } from '@app/web/trpc'
 import RichCardLabel, {
@@ -39,7 +52,9 @@ import {
 import { withTrpc } from '@app/web/components/trpc/withTrpc'
 import { yesNoBooleanOptions } from '@app/web/utils/yesNoBooleanOptions'
 import { craFormFieldsetClassname } from '@app/web/app/coop/mon-activite/cra/craFormFieldsetClassname'
-import CraBeneficiaryForm, { CraDataWithBeneficiaire } from '@app/web/app/coop/mon-activite/cra/CraBeneficiaryForm'
+import CraBeneficiaryForm, {
+  CraDataWithBeneficiaire,
+} from '@app/web/app/coop/mon-activite/cra/CraBeneficiaryForm'
 import { encodeSerializableState } from '@app/web/utils/encodeSerializableState'
 import type { BeneficiaireData } from '@app/web/beneficiaire/BeneficiaireValidation'
 import { banMunicipalityLabel } from '@app/web/external-apis/ban/banMunicipalityLabel'
