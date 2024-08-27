79b1b97868dca04c7080279bcafb673110970915
Marc Gavanier <marc.gavanier@gmail.com>
2024-08-27 10:26:15 +0200

    feat: add prise rdv input hint

--- apps/web/src/components/structure/fields/InformationsPratiquesFields.tsx
+++ apps/web/src/components/structure/fields/InformationsPratiquesFields.tsx
@@ -49,6 +49,7 @@ export const InformationsPratiquesFields = <
               href="https://acceslibre.beta.gouv.fr"
               target="_blank"
               className="fr-link fr-link--xs"
+              title="Site d’accès libre (nouvel onglet)"
             >
               accès libre
             </Link>{' '}
@@ -62,7 +63,22 @@ export const InformationsPratiquesFields = <
       <InputFormField
         path="priseRdv"
         label="Prise de rendez-vous en ligne"
-        placeholder="https://www.rdv-aide-numerique.fr/..."
+        hint={
+          <>
+            Si la structure dispose d’un outil en ligne de prise de rendez-vous
+            (par exemple RDV Aide Numérique{' '}
+            <Link
+              href="https://www.rdv-aide-numerique.fr/presentation_agent"
+              target="_blank"
+              className="fr-link fr-link--xs"
+              title="Site de RDV Aide Numérique (nouvel onglet)"
+            >
+              https://www.rdv-aide-numerique.fr/presentation_agent
+            </Link>
+            ) , vous pouvez ajouter le lien ici.
+          </>
+        }
+        placeholder="https://..."
         control={control}
         disabled={formState.isSubmitting}
       />
