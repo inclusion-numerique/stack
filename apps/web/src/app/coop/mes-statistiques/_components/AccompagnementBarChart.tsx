e106d9c13b0e21add62a96796c5fdf9351474fe3
Thibault <thibault.rouveyrol@gmail.com>
2024-09-19 19:45:17 +0200

    fix: fixing barchart on stats

--- apps/web/src/app/coop/mes-statistiques/_components/AccompagnementBarChart.tsx
+++ apps/web/src/app/coop/mes-statistiques/_components/AccompagnementBarChart.tsx
@@ -35,21 +35,31 @@ export const AccompagnementBarChart = ({
   <ResponsiveContainer width="100%" height={200}>
     <BarChart
       data={data}
-      margin={{ top: 17, right: 30, left: -20, bottom: 0 }}
+      margin={{ top: 5, right: 30, left: -20, bottom: 10 }}
       barSize={16}
     >
       <XAxis
+        className="fr-text--sm fr-text--medium"
         dataKey="label"
         scale="point"
-        tickMargin={10}
+        tick={{ dy: 10 }}
         padding={{ left: 10, right: 10 }}
+        tickLine={false}
+        angle={-45}
+        interval={data.length > 12 ? 2 : 0}
       />
-      <YAxis tickMargin={10} allowDecimals={false} />
+      <YAxis
+        className="fr-text--sm fr-text--medium"
+        tickMargin={10}
+        allowDecimals={false}
+        interval="preserveStartEnd"
+      />
       <Tooltip content={<CustomTooltip />} />
       <Bar dataKey="count" fill="#009099" radius={[10, 10, 0, 0]}>
         <LabelList
           dataKey="count"
           position="top"
+          style={{ fontSize: 14, fontWeight: 'bold' }}
           formatter={(count: number) => (count === 0 ? '' : count)}
         />
       </Bar>
