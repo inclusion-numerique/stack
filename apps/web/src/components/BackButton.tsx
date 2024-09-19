8d01b59828e155e8cc74cd0dea48fc1fc078c9d7
Hugues Maignol <hugues@maignol.com>
2024-09-19 11:02:21 +0200

    feat: back button component

---
+++ apps/web/src/components/BackButton.tsx
@@ -0,0 +1,29 @@
+import Button from '@codegouvfr/react-dsfr/Button'
+import React, { ReactNode } from 'react'
+import classNames from 'classnames'
+
+const BackButton = ({
+  href,
+  children = 'Retour',
+  className,
+}: {
+  href: string
+  children?: ReactNode
+  className?: string
+}) => {
+  return (
+    <Button
+      priority="tertiary no outline"
+      size="small"
+      linkProps={{
+        href,
+      }}
+      className={classNames('fr-mt-4v fr-mb-4v', className)}
+      iconId="fr-icon-arrow-left-line"
+    >
+      {children}
+    </Button>
+  )
+}
+
+export default BackButton
