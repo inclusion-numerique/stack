9d5f76ba482bf8c55ce83974409018d50a0953ed
Hugues Maignol <hugues@maignol.com>
2024-10-01 17:39:25 +0200

    fix: e2e cypress log dir failure in ci

--- packages/e2e/cypress/support/logToFile.ts
+++ packages/e2e/cypress/support/logToFile.ts
@@ -1,11 +1,15 @@
 import fs from 'node:fs'
 import path from 'node:path'
 
-export const logFile = path.resolve(
-  import.meta.dirname,
-  '../../../../var',
-  'cypress.log',
-)
+/**
+ * Only import this file in e2e cypress task management features
+ */
+
+const logDirectory = path.resolve(import.meta.dirname, '../../../../var')
+
+export const logFile = path.resolve(logDirectory, 'cypress.log')
+
+fs.mkdirSync(logDirectory, { recursive: true })
 
 export const logToFile = (...content: unknown[]) => {
   const timestamp = new Date().toISOString()
