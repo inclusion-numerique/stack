1784ad551197aca9caa5a7c1a2f631d4eb78944d
Hugues Maignol <hugues@maignol.com>
2024-10-04 14:10:41 +0200

    chore: debug built static path

--- apps/web/src/staticDirectory.ts
+++ apps/web/src/staticDirectory.ts
@@ -1,19 +1,17 @@
 import path from 'node:path'
 import { getDirname } from '@app/config/dirname'
 
+// XXX We need to find a more robust way to do this
 export const staticDirectory = () => {
-  let dirname = getDirname(import.meta.url)
-  console.log(dirname)
-  console.log('CWD', process.cwd())
-  // TODO Different execution environments built / dev / e2e do not have the same dirname relative to staticDirectory
+  console.log('STATIC DIR DIRNAME', getDirname(import.meta.url))
+  console.log('STATIC DIR CWD', process.cwd())
 
-  if (dirname.includes('apps/web')) {
-    const root = dirname.split('apps/web')[0]
-    dirname = path.resolve(root, 'apps/web/src')
-    console.log('APPS WEB dir', dirname)
+  // For local and e2e execution environments
+  const cwd = process.cwd()
+  if (cwd.includes('apps/web')) {
+    return path.resolve(cwd.split('apps/web')[0], 'apps/web/src')
   }
-
-  return path.resolve(dirname, '../private-static')
+  return path.resolve(cwd, './private-static')
 }
 
 export const staticFile = (filename: string) =>
