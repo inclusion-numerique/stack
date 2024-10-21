707b96f487c68ae2cd2e9d5698702a786a65e49e
Hugues Maignol <hugues@maignol.com>
2024-10-21 14:57:53 +0200

    feat: add time to /health response

--- apps/web/src/app/health/route.ts
+++ apps/web/src/app/health/route.ts
@@ -5,6 +5,10 @@ export const revalidate = 0
 
 export const GET = () =>
   new Response(
-    JSON.stringify({ status: 'ok', publicConfiguration: PublicWebAppConfig }),
+    JSON.stringify({
+      status: 'ok',
+      publicConfiguration: PublicWebAppConfig,
+      time: new Date().toISOString(),
+    }),
     { headers: { 'Content-Type': 'application/json' } },
   )
