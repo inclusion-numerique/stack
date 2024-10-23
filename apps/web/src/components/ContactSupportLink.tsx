4c1f6005ef257a82e535290701c08e886405a917
Hugues Maignol <hugues@maignol.com>
2024-10-23 17:39:42 +0200

    feat: ContactSupportLink size prop

--- apps/web/src/components/ContactSupportLink.tsx
+++ apps/web/src/components/ContactSupportLink.tsx
@@ -5,13 +5,15 @@ import { PublicWebAppConfig } from '@app/web/PublicWebAppConfig'
 const ContactSupportLink = ({
   className,
   children = 'Contactez le support',
+  size = 'sm',
 }: {
   className?: string
   children?: ReactNode
+  size?: 'sm' | 'md' | 'lg' | 'xl'
 }) => (
   <a
     href={`mailto:${PublicWebAppConfig.contactEmail}`}
-    className={classNames('fr-link fr-link--sm fr-mb-0', className)}
+    className={classNames(`fr-link fr-link--${size} fr-mb-0`, className)}
   >
     {children}
   </a>
