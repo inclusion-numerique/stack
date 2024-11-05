6f0b87e8c04514beba783eb3444aabc2d6ad4eba
Hugues Maignol <hugues@maignol.com>
2024-11-05 08:59:43 +0100

    qa: typo fix

--- apps/web/src/app/coop/(sidemenu-layout)/mes-statistiques/_sections/StatistiquesBeneficiaires.tsx
+++ apps/web/src/app/coop/(sidemenu-layout)/mes-statistiques/_sections/StatistiquesBeneficiaires.tsx
@@ -16,7 +16,7 @@ const tranchesAgeColors = [
   '#E3E3FD',
 ]
 const statusColors = ['#FFF480', '#B68B65', '#A3A6BC', '#C6C6FB', '#FFDBD2']
-const grenresColors = ['#C08C65', '#009099', '#E3E3FD']
+const genresColors = ['#C08C65', '#009099', '#E3E3FD']
 const communeColor = '#6A6AF4'
 
 const toProgress = ({
@@ -97,7 +97,7 @@ export const StatistiquesBeneficiaires = ({
             </div>
             <QuantifiedShareLegend
               quantifiedShares={beneficiaires.genres}
-              colors={grenresColors}
+              colors={genresColors}
             />
           </div>
           <div className="fr-col-lg-6 fr-col-12">
@@ -105,7 +105,7 @@ export const StatistiquesBeneficiaires = ({
               size={140}
               className="fr-mx-auto"
               data={beneficiaires.genres}
-              colors={grenresColors}
+              colors={genresColors}
             />
           </div>
         </div>
