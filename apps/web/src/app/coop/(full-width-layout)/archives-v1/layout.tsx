5c3beeee1b34d6556f29c7c0ad0f18ebed29a322
Hugues Maignol <hugues@maignol.com>
2024-11-06 17:43:00 +0100

    feat: archives v1 full width layout

--- apps/web/src/app/coop/(full-width-layout)/archives-v1/layout.tsx
+++ apps/web/src/app/coop/(full-width-layout)/archives-v1/layout.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from 'react'
-import ArchivesV1Layout from '@app/web/app/coop/(sidemenu-layout)/archives-v1/ArchivesV1Layout'
+import ArchivesV1Layout from '@app/web/app/coop/(full-width-layout)/archives-v1/ArchivesV1Layout'
 
 const Layout = ({ children }: PropsWithChildren) => (
   <ArchivesV1Layout>{children}</ArchivesV1Layout>
