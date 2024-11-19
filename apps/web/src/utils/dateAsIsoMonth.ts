4739bbd1ed01cb74e81273ea266890e51872c9c9
Hugues Maignol <hugues@maignol.com>
2024-11-19 16:47:35 +0100

    feat: date as month !stack
    (cherry picked from commit c6fb92673993e261e7fb15ef45683e1f3e06e9fc)

--- apps/web/src/utils/dateAsIsoMonth.ts
+++ apps/web/src/utils/dateAsIsoMonth.ts
@@ -1,3 +1,3 @@
 import { dateFormatter } from '@app/web/utils/formatDate'
 
-export const dateAsIsoDay = dateFormatter('yyyy-MM-dd')
+export const dateAsIsoMonth = dateFormatter('yyyy-MM')
--- apps/web/src/utils/dateAsMonth.spec.ts
+++ apps/web/src/utils/dateAsMonth.spec.ts
@@ -1,7 +1,17 @@
-import { dateAsMonth } from '@app/web/utils/dateAsMonth'
+import { dateAsMonth, dateAsMonthFull } from '@app/web/utils/dateAsMonth'
 
 describe('dateAsMonth', () => {
-  it('formats date', () => {
-    expect(dateAsMonth(new Date('2023-02-16T07:48:58'))).toEqual('02/2023')
+  describe('dateAsMonth', () => {
+    it('formats date', () => {
+      expect(dateAsMonth(new Date('2023-02-16T07:48:58'))).toEqual('02/2023')
+    })
+  })
+
+  describe('dateAsMonthFull', () => {
+    it('formats date', () => {
+      expect(dateAsMonthFull(new Date('2023-02-16T07:48:58'))).toEqual(
+        'février 2023',
+      )
+    })
   })
 })
