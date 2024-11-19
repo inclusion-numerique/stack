7c9ee22304c03335d3b3dd2b97d237888986979c
Hugues Maignol <hugues@maignol.com>
2024-11-19 10:19:02 +0100

    feat: async cra v1 import routine

--- apps/web/src/jobs/import-cras-conseiller-numerique-v1/executeImportCrasConseillerNumeriqueV1.ts
+++ apps/web/src/jobs/import-cras-conseiller-numerique-v1/executeImportCrasConseillerNumeriqueV1.ts
@@ -1,4 +1,5 @@
 import { addWeeks } from 'date-fns'
+import * as Sentry from '@sentry/nextjs'
 import { output } from '@app/web/jobs/output'
 import type { ImportCrasConseillerNumeriqueV1Job } from '@app/web/jobs/import-cras-conseiller-numerique-v1/ImportCrasConseillerNumeriqueV1Job'
 import {
@@ -35,52 +36,62 @@ const cleanupAfterImport = async () => {
   await vacuumAnalyzeConseillerNumeriqueV1Cras()
 }
 
-export const executeImportCrasConseillerNumeriqueV1 = async (
+const importCrasConseillerNumeriqueV1ByWeeks = async () => {
+  // Reset table
+  await prismaClient.craConseillerNumeriqueV1.deleteMany({})
+
+  const weeks = createWeeksArrays({
+    from: firstV1CrasMonth,
+    until: new Date(),
+  })
+
+  const result = {
+    totalImported: 0,
+    weeks: weeks.length,
+    from: dateAsDay(firstV1CrasMonth),
+    until: dateAsDay(new Date()),
+  }
+
+  for (const week of weeks) {
+    const batchSince = week
+    const batchUntil = addWeeks(week, 1)
+    output.log(
+      `import-cras-conseiller-numerique-v1: importing week ${batchSince.toISOString().slice(0, 10)}`,
+    )
+    // eslint-disable-next-line no-await-in-loop
+    const weekResult = await importCrasConseillerNumeriqueV1({
+      createdAtSince: batchSince,
+      createdAtUntil: batchUntil,
+    })
+
+    result.totalImported += weekResult.created
+
+    output.log(
+      `import-cras-conseiller-numerique-v1: imported ${weekResult.empty ? 0 : weekResult.cras.length} cras in ${week.toISOString().slice(0, 10)}`,
+    )
+  }
+
+  await cleanupAfterImport()
+
+  output.log(
+    `import-cras-conseiller-numerique-v1: finished importing ${result.totalImported} cras in ${result.weeks} weeks from ${result.from} to ${result.until}`,
+  )
+
+  return result
+}
+
+/**
+ * Wrapper to launch the execution asynchronously
+ */
+export const executeImportCrasConseillerNumeriqueV1 = (
   _job: ImportCrasConseillerNumeriqueV1Job,
 ) => {
   output.log(`import-cras-conseiller-numerique-v1: importing`)
 
-  // Reset table
-  await prismaClient.craConseillerNumeriqueV1.deleteMany({})
+  // Async execution
+  importCrasConseillerNumeriqueV1ByWeeks().catch((error) => {
+    Sentry.captureException(error)
+  })
 
-  // We need to import period by period to avoid memory issues
-
-  const byWeeks = true
-
-  if (byWeeks) {
-    const weeks = createWeeksArrays({
-      from: firstV1CrasMonth,
-      until: new Date(),
-    })
-
-    const result = {
-      totalImported: 0,
-      weeks: weeks.length,
-      from: dateAsDay(firstV1CrasMonth),
-      until: dateAsDay(new Date()),
-    }
-
-    for (const week of weeks) {
-      const batchSince = week
-      const batchUntil = addWeeks(week, 1)
-      output.log(
-        `import-cras-conseiller-numerique-v1: importing week ${batchSince.toISOString().slice(0, 10)}`,
-      )
-      // eslint-disable-next-line no-await-in-loop
-      const weekResult = await importCrasConseillerNumeriqueV1({
-        createdAtSince: batchSince,
-        createdAtUntil: batchUntil,
-      })
-
-      result.totalImported += weekResult.created
-
-      output.log(
-        `import-cras-conseiller-numerique-v1: imported ${weekResult.empty ? 0 : weekResult.cras.length} cras in ${week.toISOString().slice(0, 10)}`,
-      )
-    }
-
-    await cleanupAfterImport()
-
-    return result
-  }
+  return Promise.resolve({})
 }
