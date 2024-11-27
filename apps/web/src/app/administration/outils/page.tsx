061bf456d41ab65ade5f072308c0585fe8c55a7a
Hugues Maignol <hugues@maignol.com>
2024-11-27 15:04:39 +0100

    feat: proconnect csv user export

---
+++ apps/web/src/app/administration/outils/page.tsx
@@ -0,0 +1,35 @@
+import Button from '@codegouvfr/react-dsfr/Button'
+import { metadataTitle } from '@app/web/app/metadataTitle'
+import AdministrationBreadcrumbs from '@app/web/app/administration/AdministrationBreadcrumbs'
+import AdministrationTitle from '@app/web/app/administration/AdministrationTitle'
+import CoopPageContainer from '@app/web/app/coop/CoopPageContainer'
+
+export const metadata = {
+  title: metadataTitle('Usurpation'),
+}
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
+const Page = () => (
+  <CoopPageContainer>
+    <AdministrationBreadcrumbs currentPage="Outils" />
+    <AdministrationTitle icon="fr-icon-settings-5-line">
+      Outils
+    </AdministrationTitle>
+
+    <Button
+      iconId="fr-icon-download-line"
+      title="Télécharger la liste des utisateurs de la V1 pour ProConnect"
+      priority="secondary"
+      linkProps={{
+        href: '/api/proconnect/users.csv',
+        download: true,
+      }}
+    >
+      Télécharger la liste des utisateurs de la V1 pour ProConnect
+    </Button>
+  </CoopPageContainer>
+)
+
+export default Page
---
+++ apps/web/src/app/api/proconnect/users.csv/ProconnectUserInfoApiResponse.ts
@@ -0,0 +1,10 @@
+export type ProconnectUserInfo = {
+  id: string
+  prenom: string
+  nom: string
+  emailPro: string
+  telephone: string | null
+  structureId: string
+  structureSiret: string
+  structureNom: string
+}
