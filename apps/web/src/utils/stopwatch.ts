0c38a157c079085ab5f029fc0e67e4f71fdaea96
Hugues Maignol <hugues@maignol.com>
2024-11-29 13:39:59 +0100

    feat: add check() method to stopwatch !stack

--- apps/web/src/utils/stopwatch.ts
+++ apps/web/src/utils/stopwatch.ts
@@ -1,8 +1,20 @@
 export const createStopwatch = () => {
   const started = new Date()
+  let lastCheck = started
 
   return {
     started,
+    check: () => {
+      const checked = new Date()
+      const duration = checked.getTime() - lastCheck.getTime()
+      lastCheck = checked
+      return {
+        duration,
+        started,
+        lastCheck,
+        checked,
+      }
+    },
     stop: () => {
       const ended = new Date()
       const duration = ended.getTime() - started.getTime()
