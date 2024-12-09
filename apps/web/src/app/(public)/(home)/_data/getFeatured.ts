3eb881ab20d0641faefe910d2b65d74bdb2fe0d1
Thibault <thibault.rouveyrol@gmail.com>
2024-12-09 09:43:04 +0100

    update: featured content on landing december

--- apps/web/src/app/(public)/(home)/_data/getFeatured.ts
+++ apps/web/src/app/(public)/(home)/_data/getFeatured.ts
@@ -8,36 +8,36 @@ import {
 import { profileListSelect } from '@app/web/server/profiles/getProfilesList'
 
 const featuredBaseIds = [
-  // direction-interministerielle-du-numerique-dinum
-  '7e552766-ec7a-4531-9bf4-eb81f364ac96',
-  // la-coop-de-la-mediation-numerique
-  '13c42351-4658-4f93-becf-1dac55264e01',
-  // parentalite-et-numerique
-  '543f91bc-8215-4b27-a9af-a7fb14348324',
+  // departement-du-tarn
+  '84efe987-74fe-47ac-8b2d-56f24a51db15',
+  // icom-provence
+  '403f471e-c02c-455a-bd64-5527006f945e',
+  // blog-numerique-du-pays-de-bray
+  'ef3f41ec-438c-49a6-aee0-63e170b771ed',
 ]
 
 const featuredResourceIds = [
-  // exposition-legende-urbaine-la-story-de-mon-smartphone
-  '5c24658d-a639-4878-a6b7-effbdfca7191',
-  // jeu-numetco
-  '66c217db-1f70-48fd-bb88-715344de628f',
-  // decouverte-du-tableur-avec-libre-office-calc
-  '318e2f87-14db-4074-9452-bb3d61dc82fa',
-  // diagnostiquer-son-ordinateur
-  '4577136a-fb5a-4816-b2f8-e13bcca77fe5',
-  // initiation-intelligence-artificielle-ia
-  '2770adc6-286a-4469-8115-6b631b7c0168',
-  // panorama-sur-les-reseaux-sociaux-chiffres-et-bonnes-pratiques-pour-les-comprendre-et-les-utiliser
-  'd766f682-4d63-434a-9a6c-46c4226bbe81',
+  // courts-circuits
+  '984f7bdd-9b86-4342-b016-9e8a6a7b5bc6',
+  // pictionary-3d
+  'ca9539d5-a337-45ad-8b32-9f3e3f87372d',
+  // comptoir-du-libre
+  '8188b084-f8f8-40b0-8f70-30aede46f5a4',
+  // france-identite-prouver-son-identite-en-ligne-mais-pas-que
+  '72c510fd-93e6-4250-b6ad-5791cbfad393',
+  // solutions-de-mobilites-douces-pour-l-inclusion-numerique
+  '144d7a15-6391-47db-bd8c-df8fe185ed5e',
+  // comment-aider-une-personne-a-connaitre-ses-droits
+  'b3a7e0d9-925b-42c5-8e34-a033eaabf20f',
 ]
 
 const featuredProfileIds = [
-  // louis-derrac
-  'df80840b-be80-4dfa-847b-b4246aee5c54',
-  // eric-antier
-  '434e6b82-ddb0-4ecd-992c-9aacfaf07ab7',
-  // claire-bl
-  'fce7f55b-56a1-4a3e-b80c-c0a75dd10d4d',
+  // f-salem
+  '1d5d6ba6-0b20-4a81-9d64-ced1e58223f2',
+  // julien-daudigeos
+  '5ba65f60-f4a3-48e5-94eb-4513ca670052',
+  // jeremie-daum
+  '36d2137b-286b-4362-945d-cab21a17377f',
 ]
 
 export const getFeatured = async ({ user }: { user: SessionUser | null }) => {
