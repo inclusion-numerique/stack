187b3e85d3f79ae2cc8200520ed7b351787a60cf
Hugues Maignol <hugues@maignol.com>
2024-12-13 14:44:00 +0100

    feat: components api doc

--- apps/web/src/app/api/v1/CursorPagination.ts
+++ apps/web/src/app/api/v1/CursorPagination.ts
@@ -4,6 +4,52 @@
  */
 import { z } from 'zod'
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     ListMetadata:
+ *       type: object
+ *       properties:
+ *         total_count:
+ *           description: nombre total d’éléments correspondants à la requête
+ *           type: integer
+ *           example: 10
+ *         items_per_page:
+ *           type: integer
+ *           description: nombre d’éléments par page
+ *           example: 500
+ *         total_pages:
+ *           description: nombre total de pages
+ *           type: integer
+ *           example: 2
+ *         has_next_page:
+ *           description: indique s'il existe une page suivante
+ *           type: boolean
+ *           example: true
+ *         has_prev_page:
+ *           description: indique s'il existe une page précédente
+ *           type: boolean
+ *           example: true
+ *     PaginationLinks:
+ *       type: object
+ *       properties:
+ *         self:
+ *           type: string
+ *           format: uri
+ *           description: lien vers cette page
+ *           example: "https://api.example.com/v1/activites"
+ *         next:
+ *           type: string
+ *           format: uri
+ *           description: lien vers la page suivante
+ *           example: "https://api.example.com/v1/activites"
+ *         prev:
+ *           type: string
+ *           format: uri
+ *           description: lien vers la page précédente
+ *           example: "https://api.example.com/v1/activites"
+ */
 export const JsonApiCursorPaginationQueryParamsValidation = z.object({
   page: z.object({
     size: z.coerce.number().min(1).max(500).default(500),
