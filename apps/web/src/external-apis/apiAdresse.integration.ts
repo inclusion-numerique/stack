04d334e7a91be5ac01f85961803faf0e03c2406e
Hugues Maignol <hugues@maignol.com>
2024-12-13 11:58:50 +0100

    qa: update flaky api adresse integration test

--- apps/web/src/external-apis/apiAdresse.integration.ts
+++ apps/web/src/external-apis/apiAdresse.integration.ts
@@ -10,23 +10,23 @@ describe('apiAdresse', () => {
       expect(results).toEqual([
         {
           geometry: { coordinates: [2.386_03, 48.888_467], type: 'Point' },
-          properties: {
+          properties: expect.objectContaining({
             city: 'Paris',
             citycode: '75119',
             context: '75, Paris, Île-de-France',
             district: 'Paris 19e Arrondissement',
             housenumber: '21',
             id: '75119_0427_00021',
-            importance: 0.743_06,
+            importance: expect.toBeNumber() as number,
             label: '21 Rue des Ardennes 75019 Paris',
             name: '21 Rue des Ardennes',
             postcode: '75019',
-            score: 0.976_641_818_181_818,
+            score: expect.toBeNumber() as number,
             street: 'Rue des Ardennes',
             type: 'housenumber',
             x: 654_978.73,
             y: 6_865_558.82,
-          },
+          }) as Record<string, unknown>,
           type: 'Feature',
         },
       ])
@@ -43,23 +43,23 @@ describe('apiAdresse', () => {
       const result = await searchAdresse('21 rue des ardennes, Paris')
       expect(result).toEqual({
         geometry: { coordinates: [2.386_03, 48.888_467], type: 'Point' },
-        properties: {
+        properties: expect.objectContaining({
           city: 'Paris',
           citycode: '75119',
           context: '75, Paris, Île-de-France',
           district: 'Paris 19e Arrondissement',
           housenumber: '21',
           id: '75119_0427_00021',
-          importance: 0.743_06,
+          importance: expect.toBeNumber() as number,
           label: '21 Rue des Ardennes 75019 Paris',
           name: '21 Rue des Ardennes',
           postcode: '75019',
-          score: 0.976_641_818_181_818,
+          score: expect.toBeNumber() as number,
           street: 'Rue des Ardennes',
           type: 'housenumber',
           x: 654_978.73,
           y: 6_865_558.82,
-        },
+        }) as Record<string, unknown>,
         type: 'Feature',
       })
     })
