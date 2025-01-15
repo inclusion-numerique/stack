6cd53f037688127d9e80f79dd1e66315e4387a44
Hugues Maignol <hugues@maignol.com>
2025-01-15 12:07:36 +0100

    feat: v1 extensive lists apis

--- apps/web/src/app/api/v1/bases/route.ts
+++ apps/web/src/app/api/v1/bases/route.ts
@@ -5,17 +5,31 @@ import { getServerUrl } from '@app/web/utils/baseUrl'
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
+export type ApiV1BasesResponse = {
+  data: {
+    id: string
+    slug: string
+    created: string
+    updated: string
+    deleted: string | null
+    is_public: boolean
+    title: string | null // Null if not public or deleted
+    url: string | null // Null if not public or deleted
+  }[]
+}
+
 // Returns all public bases
 export const GET = async (_request: NextRequest) => {
   const bases = await prismaClient.base.findMany({
-    where: {
-      isPublic: true,
-    },
+    // We select all bases for syncing external systems, but remove non public data we don't want to send to the client
     select: {
       id: true,
       title: true,
       slug: true,
       deleted: true,
+      created: true,
+      updated: true,
+      isPublic: true,
     },
     orderBy: {
       created: 'desc',
@@ -23,14 +37,20 @@ export const GET = async (_request: NextRequest) => {
   })
 
   const response = {
-    data: bases.map((base) => ({
-      id: base.id,
-      title: base.title,
-      slug: base.slug,
-      deleted: base.deleted?.toISOString(),
-      url: getServerUrl(`/bases/${base.slug}`, true),
-    })),
-  }
+    data: bases.map((base) => {
+      const obfuscate = !base.isPublic || base.deleted
+      return {
+        id: base.id,
+        slug: base.slug,
+        created: base.created.toISOString(),
+        updated: base.updated.toISOString(),
+        deleted: base.deleted?.toISOString() ?? null,
+        is_public: base.isPublic,
+        title: obfuscate ? null : base.title,
+        url: obfuscate ? null : getServerUrl(`/bases/${base.slug}`, true),
+      }
+    }),
+  } satisfies ApiV1BasesResponse
 
   return NextResponse.json(response, {
     headers: {
--- apps/web/src/app/api/v1/resources/route.ts
+++ apps/web/src/app/api/v1/resources/route.ts
@@ -5,19 +5,33 @@ import { getServerUrl } from '@app/web/utils/baseUrl'
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
+export type ApiV1ResourcesResponse = {
+  data: {
+    id: string
+    slug: string
+    title: string | null // null if not publicly visible
+    deleted: string | null
+    created: string
+    updated: string
+    is_public: boolean
+    published: string | null
+    url: string | null // null if not publicly visible
+  }[]
+}
+
 // Returns all public published resources
 export const GET = async (_request: NextRequest) => {
   const resources = await prismaClient.resource.findMany({
-    where: {
-      isPublic: true,
-      published: { not: null },
-    },
+    // We select all ressources for syncing external systems, but remove non public data we don't want to send to the client
     select: {
       id: true,
       title: true,
       slug: true,
       published: true,
+      created: true,
+      updated: true,
       deleted: true,
+      isPublic: true,
     },
     orderBy: {
       created: 'desc',
@@ -25,14 +39,24 @@ export const GET = async (_request: NextRequest) => {
   })
 
   const response = {
-    data: resources.map((resource) => ({
-      id: resource.id,
-      title: resource.title,
-      slug: resource.slug,
-      deleted: resource.deleted?.toISOString(),
-      url: getServerUrl(`/ressources/${resource.slug}`, true),
-    })),
-  }
+    data: resources.map((resource) => {
+      const obfuscate =
+        !resource.isPublic || resource.deleted || !resource.published
+      return {
+        id: resource.id,
+        slug: resource.slug,
+        title: obfuscate ? null : resource.title,
+        created: resource.created.toISOString(),
+        updated: resource.updated.toISOString(),
+        deleted: resource.deleted?.toISOString() ?? null,
+        is_public: resource.isPublic ?? false,
+        published: resource.published?.toISOString() ?? null,
+        url: obfuscate
+          ? null
+          : getServerUrl(`/ressources/${resource.slug}`, true),
+      }
+    }),
+  } satisfies ApiV1ResourcesResponse
 
   return NextResponse.json(response, {
     headers: {
