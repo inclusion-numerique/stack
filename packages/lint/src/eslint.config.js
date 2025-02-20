b17e15f0d33f9298292c8233e84e89a3a34cc364
Kevin GALLET <kevingallet@gmail.com>
2025-02-20 12:00:41 +0100

    working with all the plugins

--- packages/lint/src/eslint.config.js
+++ packages/lint/src/eslint.config.js
@@ -26,12 +26,12 @@ export default [
   ...fixupConfigRules(
     compat.extends(
       'turbo',
-      // 'airbnb/base',
-      // 'airbnb/hooks',
-      // 'plugin:eslint-comments/recommended',
-      // 'plugin:import/recommended',
-      // 'plugin:promise/recommended',
-      // 'plugin:unicorn/recommended',
+      'airbnb/base',
+      'airbnb/hooks',
+      'plugin:eslint-comments/recommended',
+      'plugin:import/recommended',
+      'plugin:promise/recommended',
+      'plugin:unicorn/recommended',
       'prettier',
     ),
   ),
@@ -52,81 +52,81 @@ export default [
       },
     },
 
-    // rules: {
-    //   'unicorn/prefer-module': 'off',
-    //   'no-irregular-whitespace': 'off',
-    //   'no-restricted-syntax': 'off',
-    //   'import/prefer-default-export': 'off',
-    //   'unicorn/no-array-callback-reference': 'off',
-    //   'unicorn/prefer-query-selector': 'off',
-    //   'no-continue': 'off',
-    //   'prettier/prettier': 'error',
+    rules: {
+      'unicorn/prefer-module': 'off',
+      'no-irregular-whitespace': 'off',
+      'no-restricted-syntax': 'off',
+      'import/prefer-default-export': 'off',
+      'unicorn/no-array-callback-reference': 'off',
+      'unicorn/prefer-query-selector': 'off',
+      'no-continue': 'off',
+      'prettier/prettier': 'error',
 
-    //   'no-unused-vars': [
-    //     'error',
-    //     {
-    //       argsIgnorePattern: '^_',
-    //       varsIgnorePattern: '^_',
-    //       caughtErrorsIgnorePattern: '^_',
-    //     },
-    //   ],
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
 
-    //   'unicorn/no-null': 'off',
+      'unicorn/no-null': 'off',
 
-    //   'unicorn/filename-case': [
-    //     'error',
-    //     {
-    //       cases: {
-    //         camelCase: true,
-    //         pascalCase: true,
-    //       },
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: {
+            camelCase: true,
+            pascalCase: true,
+          },
 
-    //       ignore: ['.json$'],
-    //     },
-    //   ],
+          ignore: ['.json$'],
+        },
+      ],
 
-    //   'unicorn/prevent-abbreviations': [
-    //     'error',
-    //     {
-    //       allowList: {
-    //         Props: true,
-    //         props: true,
-    //         args: true,
-    //         Param: true,
-    //         param: true,
-    //         Params: true,
-    //         params: true,
-    //         Env: true,
-    //         env: true,
-    //         ref: true,
-    //         Ref: true,
-    //         refs: true,
-    //         Refs: true,
-    //         doc: true,
-    //         Doc: true,
-    //         docs: true,
-    //         Docs: true,
-    //         res: true,
-    //         varDirectory: true,
-    //         createVarDirectory: true,
-    //       },
-    //     },
-    //   ],
-    // },
+      'unicorn/prevent-abbreviations': [
+        'error',
+        {
+          allowList: {
+            Props: true,
+            props: true,
+            args: true,
+            Param: true,
+            param: true,
+            Params: true,
+            params: true,
+            Env: true,
+            env: true,
+            ref: true,
+            Ref: true,
+            refs: true,
+            Refs: true,
+            doc: true,
+            Doc: true,
+            docs: true,
+            Docs: true,
+            res: true,
+            varDirectory: true,
+            createVarDirectory: true,
+          },
+        },
+      ],
+    },
   },
   ...fixupConfigRules(
     compat.extends(
       'turbo',
-      // 'airbnb',
-      // 'airbnb-typescript',
-      // 'airbnb/hooks',
-      // 'plugin:@typescript-eslint/recommended',
-      // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      // 'plugin:eslint-comments/recommended',
-      // 'plugin:import/recommended',
-      // 'plugin:import/typescript',
-      // 'plugin:promise/recommended',
-      // 'plugin:unicorn/recommended',
+      'airbnb',
+      'airbnb-typescript',
+      'airbnb/hooks',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'plugin:eslint-comments/recommended',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+      'plugin:promise/recommended',
+      'plugin:unicorn/recommended',
       'prettier',
     ),
   ).map((config) => ({
@@ -138,9 +138,9 @@ export default [
 
     plugins: {
       '@typescript-eslint': fixupPluginRules(typescriptEslint),
-      // import: fixupPluginRules(_import),
+      import: fixupPluginRules(_import),
       prettier,
-      // unicorn: fixupPluginRules(eslintPluginUnicorn),
+      unicorn: fixupPluginRules(eslintPluginUnicorn),
     },
 
     languageOptions: {
@@ -160,201 +160,201 @@ export default [
 
     rules: {
       'consistent-return': 'off',
-      // 'unicorn/expiring-todo-comments': 'off',
+      'unicorn/expiring-todo-comments': 'off',
 
-      // 'import/order': [
-      //   'error',
-      //   {
-      //     pathGroups: [
-      //       {
-      //         pattern: '@app/**',
-      //         group: 'external',
-      //         position: 'after',
-      //       },
-      //     ],
-      //   },
-      // ],
+      'import/order': [
+        'error',
+        {
+          pathGroups: [
+            {
+              pattern: '@app/**',
+              group: 'external',
+              position: 'after',
+            },
+          ],
+        },
+      ],
 
       'no-underscore-dangle': 'off',
       'unicorn/prefer-module': 'off',
       'no-restricted-syntax': 'off',
       'no-irregular-whitespace': 'off',
-      // 'unicorn/no-null': 'off',
-      // 'unicorn/no-array-callback-reference': 'off',
-      // 'unicorn/prefer-query-selector': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/no-array-callback-reference': 'off',
+      'unicorn/prefer-query-selector': 'off',
       'no-continue': 'off',
-      // 'import/no-unresolved': [
-      //   2,
-      //   {
-      //     ignore: ['^@app/', '^react-hook-form/dist'],
-      //   },
-      // ],
+      'import/no-unresolved': [
+        2,
+        {
+          ignore: ['^@app/', '^react-hook-form/dist'],
+        },
+      ],
 
       'import/no-extraneous-dependencies': 'off',
-      // '@typescript-eslint/unbound-method': 'off',
-      // '@typescript-eslint/no-misused-promises': 'off',
-      // '@typescript-eslint/no-floating-promises': 'warn',
-      // 'import/prefer-default-export': 'off',
+      '@typescript-eslint/unbound-method': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      'import/prefer-default-export': 'off',
 
-      // 'react/jsx-props-no-spreading': [
-      //   'error',
-      //   {
-      //     custom: 'ignore',
-      //   },
-      // ],
+      'react/jsx-props-no-spreading': [
+        'error',
+        {
+          custom: 'ignore',
+        },
+      ],
 
       'no-unused-vars': 'off',
-      // '@typescript-eslint/no-unused-vars': [
-      //   'error',
-      //   {
-      //     argsIgnorePattern: '^_',
-      //     varsIgnorePattern: '^_',
-      //     caughtErrorsIgnorePattern: '^_',
-      //   },
-      // ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
 
-      // 'react/function-component-definition': [
-      //   2,
-      //   {
-      //     namedComponents: 'arrow-function',
-      //     unnamedComponents: 'arrow-function',
-      //   },
-      // ],
+      'react/function-component-definition': [
+        2,
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
 
-      // 'react/require-default-props': 'off',
-      // 'react/react-in-jsx-scope': 'off',
+      'react/require-default-props': 'off',
+      'react/react-in-jsx-scope': 'off',
 
-      // 'unicorn/prevent-abbreviations': [
-      //   'error',
-      //   {
-      //     allowList: {
-      //       Props: true,
-      //       props: true,
-      //       args: true,
-      //       Param: true,
-      //       param: true,
-      //       Params: true,
-      //       params: true,
-      //       Env: true,
-      //       env: true,
-      //       ref: true,
-      //       Ref: true,
-      //       refs: true,
-      //       Refs: true,
-      //       doc: true,
-      //       Doc: true,
-      //       docs: true,
-      //       Docs: true,
-      //       res: true,
-      //       varDirectory: true,
-      //       createVarDirectory: true,
-      //     },
-      //   },
-      // ],
+      'unicorn/prevent-abbreviations': [
+        'error',
+        {
+          allowList: {
+            Props: true,
+            props: true,
+            args: true,
+            Param: true,
+            param: true,
+            Params: true,
+            params: true,
+            Env: true,
+            env: true,
+            ref: true,
+            Ref: true,
+            refs: true,
+            Refs: true,
+            doc: true,
+            Doc: true,
+            docs: true,
+            Docs: true,
+            res: true,
+            varDirectory: true,
+            createVarDirectory: true,
+          },
+        },
+      ],
 
-      // 'unicorn/filename-case': [
-      //   'error',
-      //   {
-      //     cases: {
-      //       camelCase: true,
-      //       pascalCase: true,
-      //     },
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: {
+            camelCase: true,
+            pascalCase: true,
+          },
 
-      //     ignore: ['.json$'],
-      //   },
-      // ],
+          ignore: ['.json$'],
+        },
+      ],
 
-      // 'unicorn/no-array-reduce': 'off',
+      'unicorn/no-array-reduce': 'off',
     },
   },
   // Spec
-  // ...fixupConfigRules(
-  //   compat.extends(
-  //     'turbo',
-  //     'airbnb',
-  //     'airbnb-typescript',
-  //     'airbnb/hooks',
-  //     'plugin:@typescript-eslint/recommended',
-  //     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  //     'plugin:eslint-comments/recommended',
-  //     'plugin:import/recommended',
-  //     'plugin:import/typescript',
-  //     'plugin:promise/recommended',
-  //     'plugin:unicorn/recommended',
-  //     'prettier',
-  //     'plugin:jest/recommended',
-  //   ),
-  // ).map((config) => ({
-  //   ...config,
-  //   files: ['**/*.+(spec|integration).+(ts|tsx)'],
-  // })),
-  // {
-  //   files: ['**/*.+(spec|integration).+(ts|tsx)'],
+  ...fixupConfigRules(
+    compat.extends(
+      'turbo',
+      'airbnb',
+      'airbnb-typescript',
+      'airbnb/hooks',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'plugin:eslint-comments/recommended',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+      'plugin:promise/recommended',
+      'plugin:unicorn/recommended',
+      'prettier',
+      'plugin:jest/recommended',
+    ),
+  ).map((config) => ({
+    ...config,
+    files: ['**/*.+(spec|integration).+(ts|tsx)'],
+  })),
+  {
+    files: ['**/*.+(spec|integration).+(ts|tsx)'],
 
-  //   plugins: {
-  //     jest: fixupPluginRules(jest),
-  //   },
+    plugins: {
+      jest: fixupPluginRules(jest),
+    },
 
-  //   rules: {
-  //     '@typescript-eslint/no-loop-func': 'off',
-  //     'import/no-extraneous-dependencies': 'off',
-  //     'unicorn/no-null': 'off',
-  //     'no-restricted-syntax': 'off',
-  //     'unicorn/prefer-module': 'off',
-  //   },
-  // },
+    rules: {
+      '@typescript-eslint/no-loop-func': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'unicorn/no-null': 'off',
+      'no-restricted-syntax': 'off',
+      'unicorn/prefer-module': 'off',
+    },
+  },
   // // E2E
-  // ...fixupConfigRules(
-  //   compat.extends(
-  //     'turbo',
-  //     'airbnb',
-  //     'airbnb-typescript',
-  //     'airbnb/hooks',
-  //     'plugin:@typescript-eslint/recommended',
-  //     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  //     'plugin:eslint-comments/recommended',
-  //     'plugin:import/recommended',
-  //     'plugin:import/typescript',
-  //     'plugin:promise/recommended',
-  //     'plugin:unicorn/recommended',
-  //     'prettier',
-  //     'plugin:cypress/recommended',
-  //   ),
-  // ).map((config) => ({
-  //   ...config,
-  //   files: ['./packages/e2e/**/*.+(ts|tsx)'],
-  // })),
-  // {
-  //   files: ['./packages/e2e/**/*.+(ts|tsx)'],
+  ...fixupConfigRules(
+    compat.extends(
+      'turbo',
+      'airbnb',
+      'airbnb-typescript',
+      'airbnb/hooks',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'plugin:eslint-comments/recommended',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+      'plugin:promise/recommended',
+      'plugin:unicorn/recommended',
+      'prettier',
+      'plugin:cypress/recommended',
+    ),
+  ).map((config) => ({
+    ...config,
+    files: ['./packages/e2e/**/*.+(ts|tsx)'],
+  })),
+  {
+    files: ['./packages/e2e/**/*.+(ts|tsx)'],
 
-  //   plugins: {
-  //     cypress: fixupPluginRules(cypress),
-  //   },
+    plugins: {
+      cypress: fixupPluginRules(cypress),
+    },
 
-  //   languageOptions: {
-  //     parser: tsParser,
-  //   },
+    languageOptions: {
+      parser: tsParser,
+    },
 
-  //   rules: {
-  //     'import/no-extraneous-dependencies': 'off',
-  //     'unicorn/no-null': 'off',
-  //     'import/prefer-default-export': 'off',
-  //     'no-restricted-syntax': 'off',
-  //     'promise/catch-or-return': 'off',
-  //     'promise/always-return': 'off',
-  //     'cypress/unsafe-to-chain-command': 'off',
-  //   },
-  // },
-  // // JSON
-  // ...compat.extends('plugin:json/recommended-legacy').map((config) => ({
-  //   ...config,
-  //   files: ['**/*.json'],
-  // })),
-  // {
-  //   files: ['**/*.json'],
+    rules: {
+      'import/no-extraneous-dependencies': 'off',
+      'unicorn/no-null': 'off',
+      'import/prefer-default-export': 'off',
+      'no-restricted-syntax': 'off',
+      'promise/catch-or-return': 'off',
+      'promise/always-return': 'off',
+      'cypress/unsafe-to-chain-command': 'off',
+    },
+  },
+  // JSON
+  ...compat.extends('plugin:json/recommended-legacy').map((config) => ({
+    ...config,
+    files: ['**/*.json'],
+  })),
+  {
+    files: ['**/*.json'],
 
-  //   plugins: {
-  //     json,
-  //   },
-  // },
+    plugins: {
+      json,
+    },
+  },
 ]
