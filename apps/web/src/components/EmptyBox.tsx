88dae0d365c6d07e541241373a2225307178e534
Kevin GALLET <kevingallet@gmail.com>
2025-02-27 15:14:42 +0100

    fix: data-test-id empty box e2e test

--- apps/web/src/components/EmptyBox.tsx
+++ apps/web/src/components/EmptyBox.tsx
@@ -7,14 +7,16 @@ const EmptyBox = ({
   children,
   className,
   titleAs: EmptyBoxTitle = 'h2',
+  'data-testid': dataTestId = 'empty-box',
 }: {
   title?: ReactNode
   className?: string
   titleAs?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+  'data-testid'?: string
 } & PropsWithChildren) => (
   <div
     className={classNames(styles.container, className)}
-    data-testid="empty-box"
+    data-testid={dataTestId}
   >
     {!!title && (
       <EmptyBoxTitle className="fr-mb-1w fr-h6">{title}</EmptyBoxTitle>
