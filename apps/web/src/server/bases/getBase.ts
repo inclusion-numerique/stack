2240417c94b6cf133bf20f046164224f70e7591b
Kevin GALLET <kevingallet@gmail.com>
2025-03-06 11:20:17 +0100

    feat: add orderby for base collection

--- apps/web/src/server/bases/getBase.ts
+++ apps/web/src/server/bases/getBase.ts
@@ -1,6 +1,6 @@
 import { Prisma } from '@prisma/client'
-import { prismaClient } from '@app/web/prismaClient'
 import { SessionUser } from '@app/web/auth/sessionUser'
+import { prismaClient } from '@app/web/prismaClient'
 import { imageCropSelect } from '@app/web/server/image/imageCropSelect'
 import {
   computeResourcesListWhereForUser,
@@ -84,9 +84,12 @@ export const baseSelect = (user: Pick<SessionUser, 'id'> | null) =>
     collections: {
       select: collectionSelect(user),
       where: computeCollectionsListWhereForUser(user),
-      orderBy: {
-        updated: 'desc',
-      },
+      orderBy: [
+        { order: 'asc' },
+        {
+          created: 'asc',
+        },
+      ],
     },
     savedCollections: {
       select: {
