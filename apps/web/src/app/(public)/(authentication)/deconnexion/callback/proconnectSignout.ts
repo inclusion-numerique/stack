50420df9dccc3124aa9d39d00e3c6daee49e1316
Hugues Maignol <hugues@maignol.com>
2025-03-17 17:16:58 +0100

    chore: remove state variable for proconnect logout

--- apps/web/src/app/(public)/(authentication)/deconnexion/callback/proconnectSignout.ts
+++ apps/web/src/app/(public)/(authentication)/deconnexion/callback/proconnectSignout.ts
@@ -1,6 +1,4 @@
 import { PublicWebAppConfig } from '@app/web/PublicWebAppConfig'
-import { encodeSerializableState } from '@app/web/utils/encodeSerializableState'
-import { v4 } from 'uuid'
 
 const proconnectSignoutRedirectPath = '/deconnexion/callback'
 
@@ -16,22 +14,23 @@ export type ProconnectSignoutState = {
  */
 export const generateProconnectSignoutUrl = ({
   origin,
-  callbackUrl,
+  callbackUrl: _callbackUrl,
   idTokenHint,
 }: {
   origin: string
   idTokenHint: string
   callbackUrl: string
 }) => {
-  const state = encodeSerializableState({
-    callbackUrl,
-    nonce: v4(),
-  })
+  // ProConnect does not support dynamic state parameter so we remove state from the url
+  // https://github.com/numerique-gouv/proconnect-documentation/blob/main/doc_fs/implementation_technique.md#243-vérification-du-state
+  // const state = encodeSerializableState({
+  //   callbackUrl,
+  //   nonce: v4(),
+  // })
 
   const postLogoutRedirectUri = `${origin}${proconnectSignoutRedirectPath}`
 
   const queryParams = new URLSearchParams({
-    state,
     post_logout_redirect_uri: postLogoutRedirectUri,
     id_token_hint: idTokenHint,
   })
