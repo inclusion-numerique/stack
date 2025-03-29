fb29421a7fbacbc3e46d33ece283983bdc53097c
Hugues Maignol <hugues@maignol.com>
2025-03-29 17:04:59 +0100

    qa: update embeddings integration test

--- apps/web/src/assistant/rag/getRagChunksForQuery.integration.ts
+++ apps/web/src/assistant/rag/getRagChunksForQuery.integration.ts
@@ -1,8 +1,8 @@
-import { openAiClientConfiguration } from '@app/web/assistant/openAiClient'
+import { aiSdkAlbertEmbeddingModel } from '@app/web/assistant/aiSdkAlbertProvider'
 import { getRagChunksForQuery } from '@app/web/assistant/rag/getRagChunksForQuery'
 import { prismaClient } from '@app/web/prismaClient'
-import huitreEmbedding from './testing.huitre.embedding.json'
-import zebreEmbedding from './testing.zebre.embedding.json'
+import huitreEmbedding from './_test/testing.huitre.embedding.json'
+import zebreEmbedding from './_test/testing.zebre.embedding.json'
 
 describe('getRagChunksForQuery', () => {
   beforeAll(async () => {
@@ -20,7 +20,7 @@ describe('getRagChunksForQuery', () => {
           type: 'test',
           documentMd5: 'test',
           url: `https://test/${testEmbedding.name}`,
-          embeddingModel: openAiClientConfiguration.embeddingsModel,
+          embeddingModel: aiSdkAlbertEmbeddingModel.modelId,
           content: 'test',
           chunk: 0,
           sourceId: testEmbedding.name,
@@ -58,7 +58,7 @@ describe('getRagChunksForQuery', () => {
         created: expect.toBeDateString() as string,
         updated: expect.toBeDateString() as string,
         documentMd5: 'test',
-        embeddingModel: openAiClientConfiguration.embeddingsModel,
+        embeddingModel: aiSdkAlbertEmbeddingModel.modelId,
         similarity: expect.toBeNumber() as number,
         source: 'test',
         type: 'test',
@@ -72,7 +72,7 @@ describe('getRagChunksForQuery', () => {
         created: expect.toBeDateString() as string,
         updated: expect.toBeDateString() as string,
         documentMd5: 'test',
-        embeddingModel: openAiClientConfiguration.embeddingsModel,
+        embeddingModel: aiSdkAlbertEmbeddingModel.modelId,
         similarity: expect.toBeNumber() as number,
         source: 'test',
         type: 'test',
