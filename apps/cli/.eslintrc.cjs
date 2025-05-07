8c01c5a015d816d7d9c60a922c7ec7e39978f8a6
Hugues Maignol <hugues@maignol.com>
2025-05-07 10:35:39 +0200

    chore: switch to biome for linting

--- apps/cli/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rules: {
-    // Process exit ok in cli
-    'unicorn/no-process-exit': 'off',
-  },
-  parserOptions: {
-    project: 'tsconfig.json',
-  },
-}
--- packages/lint/jest.config.ts
+++
@@ -1,3 +0,0 @@
-import { packageJestConfig } from '../../packages/test/src/packageJestConfig'
-
-export default packageJestConfig({})
