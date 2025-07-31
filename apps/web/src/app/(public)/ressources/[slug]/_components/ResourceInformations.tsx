2108ffee5e3c85f418b5445ed8c6b91e3e9dd9d9
Kevin GALLET <kevingallet@gmail.com>
2025-07-31 10:17:52 +0200

    feat: add feedback in resource footer

--- apps/web/src/app/(public)/ressources/[slug]/_components/ResourceInformations.tsx
+++ apps/web/src/app/(public)/ressources/[slug]/_components/ResourceInformations.tsx
@@ -1,8 +1,10 @@
 import { sPluriel } from '@app/ui/utils/pluriel/sPluriel'
 import ResourceCollectionsModal from '@app/web/app/(public)/ressources/[slug]/_components/ResourceCollectionsModal'
+import { FeedbackBadge } from '@app/web/components/Resource/feedbackBadge/FeedbackBadge'
 import ResourceLicenceInformations from '@app/web/features/resources/licence/components/ResourceLicenceInformations'
 import type { Resource } from '@app/web/server/resources/getResource'
 import Accordion from '@codegouvfr/react-dsfr/Accordion'
+import Link from 'next/link'
 import React from 'react'
 import ResourceIndexationView from './ResourceIndexationView'
 
@@ -23,6 +25,28 @@ const ResourceInformations = ({ resource }: { resource: Resource }) => (
           </>
         )}
       </div>
+      {resource._count.resourceFeedback > 0 && (
+        <>
+          <div className="fr-flex fr-align-items-center fr-flex-gap-2v">
+            <span
+              className="ri-emotion-line fr-text-label--blue-france"
+              aria-hidden
+            />
+            <span className="fr-text--sm fr-mb-0">
+              <strong>{resource._count.resourceFeedback}</strong> avis sur la
+              ressource
+            </span>
+            <FeedbackBadge value={resource.feedbackAverage} />
+            <span className="fr-hidden fr-unhidden-sm">·</span>
+            <Link
+              className="fr-link fr-text--sm fr-mb-0"
+              href={`/ressources/${resource.slug}/avis`}
+            >
+              Voir les avis
+            </Link>
+          </div>
+        </>
+      )}
       <ResourceIndexationView
         resource={resource}
         withLink
